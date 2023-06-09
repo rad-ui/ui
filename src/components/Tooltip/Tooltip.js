75aa3b34c5b63690359812d1c83122f633e213ec
Pranay Kothapalli <pranay@sprinto.com>
2023-06-09 14:06:50 +0530

    add: tooltip component

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -1,10 +1,25 @@
-'use client'
-const Tooltip = ()=>{
-    return(
-        <div>
-            <h1>Tooltip</h1>
-        </div>
-    )
-}
+import * as Tooltip from '@radix-ui/react-tooltip';
 
-export default Tooltip;
+
+const TooltipDemo = () => {
+    const DELAY=500
+  return (
+    <Tooltip.Provider delayDuration={DELAY} >
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild delayDuration={DELAY}>
+          <button className="IconButton">
+            icon here
+          </button>
+        </Tooltip.Trigger>
+        <Tooltip.Portal>
+          <Tooltip.Content className="TooltipContent" sideOffset={5}>
+            Add to library
+            <Tooltip.Arrow style={{fill:"red"}} />
+          </Tooltip.Content>
+        </Tooltip.Portal>
+      </Tooltip.Root>
+    </Tooltip.Provider>
+  );
+};
+
+export default TooltipDemo;
