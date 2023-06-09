b60f5da3999b2c22d1dcf8430d8cd918648e5cb1
Pranay Kothapalli <pranay@sprinto.com>
2023-06-09 15:26:12 +0530

    add: sample color

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Button = ({ onClick, children }) => {
   return (
-    <button className="px-2 py-2" onClick={onClick}>
+    <button className="px-2 py-2 bg-black text-white" onClick={onClick}>
       {children}
     </button>
   );
