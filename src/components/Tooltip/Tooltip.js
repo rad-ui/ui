4329e189b552d1d96818c812c7f0aa2fe366b8fb
Pranay Kothapalli <pranay@sprinto.com>
2023-06-09 13:07:24 +0530

    add: storybook 7 + sample stories

---
+++ src/components/Tooltip/Tooltip.js
@@ -0,0 +1,10 @@
+'use client'
+const Tooltip = ()=>{
+    return(
+        <div>
+            <h1>Tooltip</h1>
+        </div>
+    )
+}
+
+export default Tooltip;
---
+++ src/components/Tooltip/Tooltip.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import  Tooltip  from './Tooltip';
+
+export default {
+  title: 'Components/Tooltip',
+  component: Tooltip,
+};
+
+export const Default = () => <Tooltip>Primary Button</Tooltip>;
+
