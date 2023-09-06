ca8530df51b910dabd3d903029bb7e3c1033d6f3
Pranay <pranay@sprinto.com>
2023-09-06 10:25:52 +0530

    add: dark mode gray palette automa

--- scripts/colorGen.js
+++ scripts/colorGen.js
@@ -2,26 +2,43 @@ import fs from "fs"
 
 import colors from '../src/colors/index.js';
 
+const COLOR_PREFIX = '--rad-ui-color-';
+const jsVariables = {};
 // Function to generate CSS variables based on the theme
 function generateCssVariables(theme) {
     const cssVariables = [];
-    const themeColors = colors.gray[theme];
-  
-    for (const key in themeColors) {
-      const colorName = `gray${key.replace('gray', '')}`;
-      const colorValue = themeColors[key];
-      cssVariables.push(`--${colorName}: ${colorValue};`);
+   
+    
+    // Loop through the colors
+    for (const [colorName, colorShades] of Object.entries(colors)) {
+      // Loop through the shades
+      for (const [shadeName, shadeValue] of Object.entries(colorShades[theme])) {
+        // Create the CSS variable name
+        const cssVariableName = `${COLOR_PREFIX}${colorName}-${shadeName}`;
+        // Add the CSS variable to the array
+        cssVariables.push(`${cssVariableName}: ${shadeValue};`);
+
+        // Create the JS variable name
+        const jsVariableName = `${colorName}`;
+        // Add the JS variable to the object
+        jsVariables[jsVariableName] = jsVariables[jsVariableName] || {};
+        jsVariables[jsVariableName][shadeName] = `var(--${cssVariableName})`;
+
+      }
     }
-  
+
+    console.log(jsVariables)
+
+    // Return the CSS variables as a string
     return cssVariables.join("\n");
   }
   
 
 // Generate CSS variables for the light theme
-const lightThemeCss = generateCssVariables("light", "light-");
+const lightThemeCss = generateCssVariables("light");
 
 // Generate CSS variables for the dark theme
-const darkThemeCss = generateCssVariables("dark", "dark-");
+const darkThemeCss = generateCssVariables("dark");
 
 // Combine light and dark theme CSS
 const combinedCss = `
@@ -29,12 +46,15 @@ const combinedCss = `
     ${lightThemeCss}
   }
 
-  .dark-theme {
+  .rad-ui-dark-theme {
     ${darkThemeCss}
   }
 `;
 
 // Save the CSS to a file
-fs.writeFileSync('scripts/output/theme.css', combinedCss, 'utf-8');
+fs.writeFileSync('styles/cssTokens/base.tokens.css', combinedCss, 'utf-8');
+
+// write the JS variables to a js file
+fs.writeFileSync('styles/jsTokens/base.tokens.js', `export default ${JSON.stringify(jsVariables)}`, 'utf-8');
 
 console.log('CSS file saved as theme.css');
