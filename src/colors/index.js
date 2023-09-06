d1b9a631ca16e73233d0211a797b1a412fbc5cfa
Pranay <pranay@sprinto.com>
2023-09-06 21:44:37 +0530

    add: sage

--- src/colors/index.js
+++ src/colors/index.js
@@ -1,9 +1,13 @@
 import gray from './gray.js';
 import mauve from './mauve.js';
+import slate from './slate.js';
+import sage from './sage.js';
 
 const colors = {
     gray:gray,
-    mauve:mauve
+    mauve:mauve,
+    slate:slate,
+    sage:sage,
 }
 
 
