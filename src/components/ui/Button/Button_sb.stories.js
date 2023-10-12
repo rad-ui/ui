dfa19d98c1dcc770962e82ac177e5e80382d27a6
Pranay <pranay@sprinto.com>
2023-10-12 11:51:18 +0530

    Delete Button.stories.js, Create Button_sb.stories.js, Delete css.variables.mock.css, Delete DesignSystem.stories.js

---
+++ src/components/ui/Button/Button_sb.stories.js
@@ -0,0 +1,26 @@
+import  Button  from './Button';
+import "./css.variables.mock.css"
+import SandboxEditor from "@/components/tools/SandboxEditor/SandboxEditor"
+
+
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'UI/Input/Button',
+  component: Button,
+  render: (args) => <SandboxEditor>
+   <div className='flex space-x-2'>
+   
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="tertiary">Tertiary</Button>
+   </div>
+  </SandboxEditor>,
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Storybook = {
+  args: {
+    className:''
+  },
+};
--- src/examples/Colors/Colors.stories.js
+++ src/examples/Colors/Colors.stories.js
@@ -36,16 +36,6 @@ const AllColorsTemplate = ()=>{
 export default {
   title: 'Example/Colors',
   component: 'AllColorsTemplate',
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'centered',
-  },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
   render: (args) => <AllColorsTemplate/>,
 };
 
