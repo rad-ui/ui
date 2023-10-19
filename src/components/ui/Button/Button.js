dc2fa4596ac7295fd535ad191655968fff8fce5c
Pranay Kothapalli <pranay@sprinto.com>
2023-10-19 15:29:17 +0530

    Pranay/generate accent tokens (#35)

--- src/components/ui/Button/Button.js
+++ src/components/ui/Button/Button.js
@@ -1,6 +1,12 @@
 import React from 'react';
+// make the color prop default accent color
+const Button = ({children, type="button", color=undefined, className="", isDark=false, variant="solid",...props}) => {
 
-const Button = ({children, type="button",className="", isDark=false, variant="solid",...props}) => {
+    // apply data attribute for accent color
+    // apply attribute only if color is present
+    if(color){
+        props["data-accent-color"] = color
+    }
 
     return (
         <button
