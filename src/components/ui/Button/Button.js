ddebd5d5dd753d14c7e09c214a325103c92dfeab
Pranay <pranay@sprinto.com>
2023-10-19 09:07:19 +0530

    Make color prop default accent color

--- src/components/ui/Button/Button.js
+++ src/components/ui/Button/Button.js
@@ -1,11 +1,14 @@
 import React from 'react';
+// make the color prop default accent color
+const Button = ({children, type="button", color="tomato", className="", isDark=false, variant="solid",...props}) => {
 
-const Button = ({children, type="button",className="", isDark=false, variant="solid",...props}) => {
+    // apply data attribute for accent color
 
     return (
         <button
             type="button"
             className={`rad-ui-button button-${variant} ${className}`}
+            data-accent-color={color}
             {...props}
         >
             {children}
