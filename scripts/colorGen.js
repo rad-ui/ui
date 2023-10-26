4233d42a5d91c7b9bc67a9200f57c21f0e2c7358
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-10-26 07:29:30 +0530

    Improving build processes for component exports (#8)

--- scripts/colorGen.js
+++ scripts/colorGen.js
@@ -4,6 +4,10 @@ import colors from '../src/colors/index.js';
 
 const COLOR_PREFIX = '--rad-ui-color-';
 let jsVariables = {};
+
+let accentColor = colors.tomato;
+
+
 // Function to generate CSS variables based on the theme
 function generateCssVariables(theme) {
     const cssVariables = [];
@@ -27,11 +31,36 @@ function generateCssVariables(theme) {
       }
     }
 
-    console.log(jsVariables)
+
 
     // Return the CSS variables as a string
     return cssVariables.join("\n");
   }
+
+
+  const generateAccentColors = (theme)=>{
+    const cssVariables = [];
+    const color = accentColor;
+
+    // Loop through the shades
+    for (const [shadeName, shadeValue] of Object.entries(color[theme])) {
+      // Create the CSS variable name
+      const cssVariableName = `${COLOR_PREFIX}accent-${shadeName}`;
+      // Add the CSS variable to the array
+      cssVariables.push(`${cssVariableName}: ${shadeValue};`);
+
+      // Create the JS variable name
+      const jsVariableName = `accent`;
+      // Add the JS variable to the object
+      jsVariables[jsVariableName] = jsVariables[jsVariableName] || {};
+      jsVariables[jsVariableName][shadeName] = `var(${cssVariableName})`;
+    }
+
+    // Return the CSS variables as a string
+    return cssVariables.join("\n");
+  }
+
+  console.log(generateAccentColors("light"))
   
 
 // Generate CSS variables for the light theme
@@ -40,14 +69,19 @@ const lightThemeCss = generateCssVariables("light");
 // Generate CSS variables for the dark theme
 const darkThemeCss = generateCssVariables("dark");
 
+const accentLightCss = generateAccentColors("light");
+const accentDarkCss = generateAccentColors("dark");
+
 // Combine light and dark theme CSS
 const combinedCss = `
   :root {
     ${lightThemeCss}
+    ${accentLightCss}
   }
 
   .rad-ui-dark-theme {
     ${darkThemeCss}
+    ${accentDarkCss}
   }
 `;
 
--- src/components/ui/Button/Button.js
+++ src/components/ui/Button/Button.js
@@ -1,4 +1,9 @@
-const Button = ({children,type="button",className="", isDark=false, variant="secondary",...props}) => {
+import React from 'react';
+
+const Button = ({children, type="button",className="", isDark=false, variant="primary",...props}) => {
+    if(variant==="primary"){
+        variant = "accent"
+    }
     return (
         <button
             type="button"
