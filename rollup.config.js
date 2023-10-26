295a6aca0b26706b4aa31577d47ad373954849f7
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-10-26 07:29:30 +0530

    add: rollup script for npm publish

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+// rollup.config.js
+import babel from '@rollup/plugin-babel';
+
+export default {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/rad-ui.js',
+      format: 'umd',
+      name: 'RadUI',
+      globals: {
+        react: 'React',
+      },
+    },
+    external: ['react'],
+    plugins: [
+        babel({
+          babelHelpers: 'bundled',
+          exclude: 'node_modules/**',
+          presets: ['@babel/preset-react'], // Add this line
+        }),
+      ],
+  };
