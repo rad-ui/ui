2f7b19841856e9def577eb0109e8bdc273ce3518
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-10-26 07:29:29 +0530

    reset

--- src/components/Text/variants/index.js
+++
@@ -1,6 +0,0 @@
-export {default as Header } from './Header.js';
-export {default as H1} from './H1.js';
-export {default as H2} from './H2.js';
-export {default as H3} from './H3.js';
-export {default as H4} from './H4.js';
-export {default as Body} from './Body.js';
