4e9ffe5bd4152e0a18cb00628a889ff81490881c
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-10-27 13:52:27 +0530

    Update tailwind.config.js with new import and content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 
-import radUIColors from "./styles/jsTokens/base.tokens.js"
+import radUIColors from './styles/jsTokens/base.tokens.js';
 
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      colors:{
-        ...radUIColors
-      }
+    content: ['./src/**/*.{html,js,jsx,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                ...radUIColors,
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
 
