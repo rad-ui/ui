7bf5d124572ca10895f751e272a11282915b3a04
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-10-27 19:09:41 +0530

    Refactor Code.js, Code.stories.js, Link.js, and Quote.js

--- src/components/ui/Quote/Quote.js
+++ src/components/ui/Quote/Quote.js
@@ -1,6 +1,5 @@
-const Quote = ({ quote, author }) => {
-    return <q>{quote} <cite>{author}</cite></q>
-
-}
+const Quote = ({quote, author}) => {
+    return <q>{quote} <cite>{author}</cite></q>;
+};
 
 export default Quote;
