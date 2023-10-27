56313f1f9be760fcdd657debc08a7f08e4e62fac
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-10-27 19:09:49 +0530

    Add RENDER_AS_ENUMS to Heading component

--- src/components/ui/Heading/Heading.js
+++ src/components/ui/Heading/Heading.js
@@ -1,5 +1,32 @@
-const Heading = ({children})=>{
-    return <h1>{children}</h1>
-}
+const RENDER_AS_ENUMS = [
+    {
+        label: 'H1',
+        tag: 'h1',
+    },
+    {
+        label: 'H2',
+        tag: 'h2',
+    },
+    {
+        label: 'H3',
+        tag: 'h3',
+    },
+    {
+        label: 'H4',
+        tag: 'h4',
+    },
+    {
+        label: 'H5',
+        tag: 'h5',
+    },
+    {
+        label: 'H6',
+        tag: 'h6',
+    },
+];
 
-export default Heading
+const Heading = ({children})=>{
+    return <h1>{children}</h1>;
+};
+
+export default Heading;
