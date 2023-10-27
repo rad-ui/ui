c2c1f5bae79110d71f7bebaa613f90cda2a9c55c
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-10-27 20:23:48 +0530

    Refactor Text component and add Text.stories file

--- src/components/ui/Text/Text.js
+++ src/components/ui/Text/Text.js
@@ -1,5 +1,13 @@
-const Text = ({ children, ...props }) => {
-    return <p>{children}</p>
-}
+'use client';
+import {customClassSwitcher} from '@/core';
 
-export default Text
+const COMPONENT_NAME = 'Text';
+
+const Text = ({children, customRootClass = '', className = '', ...props}) => {
+    const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME);
+    return <p className={`${rootClass} ${className}`} {...props}>{children}</p>;
+};
+
+Text.displayName = COMPONENT_NAME;
+
+export default Text;
