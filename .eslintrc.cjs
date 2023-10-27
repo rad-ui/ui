afea4d96e2b66d2bfdb2ad93cc489b273491106f
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-10-27 11:23:56 +0530

    Add module-resolver plugin to .babelrc.json and babel preset react to .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,33 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-    'node': true,
-  },
-  'extends': [
-    'plugin:react/recommended',
-    'eslint:recommended',
-    'google',
-  ],
-  'overrides': [
-  ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
-  },
-  'plugins': [
-    'react',
-  ],
-  'rules': {
-    'react/react-in-jsx-scope': 'off',
-    'no-multiple-empty-lines': ['error', {'max': 1, 'maxEOF': 1}],
-    'semi': [
-      'error',
-      'always',
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true,
+    },
+    'extends': [
+        'plugin:react/recommended',
+        'eslint:recommended',
+        'google',
     ],
-  },
+    'overrides': [
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module',
+    },
+    'plugins': [
+        'react',
+    ],
+    'rules': {
+        'react/react-in-jsx-scope': 'off',
+        'no-multiple-empty-lines': ['error', {'max': 1, 'maxEOF': 1}],
+        'indent': ['error', 4],
+
+        // ignore long strings
+        'max-len': 'off',
+        'semi': [
+            'error',
+            'always',
+        ],
+    },
 };
