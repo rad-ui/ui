88c21d1a12d307e317aa61ca97f6ad1555243662
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-10-27 20:02:25 +0530

    Add Em component story with SandboxEditor and Heading

---
+++ src/components/ui/Em/Em.stories.js
@@ -0,0 +1,28 @@
+import Em from './Em';
+import {Heading} from '@/';
+import SandboxEditor from '@/components/tools/SandboxEditor/SandboxEditor';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'UI/Typography/Em',
+    component: Em,
+    render: (args) => <SandboxEditor>
+        <div >
+            <div className='flex space-x-2'>
+
+                <Heading className="text-gray-1000">
+                    Hello <Em className='text-gray-950'>World,</Em> How you doin?
+                </Heading>
+
+            </div>
+
+        </div>
+    </SandboxEditor>,
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const All = {
+    args: {
+        className: '',
+    },
+};
