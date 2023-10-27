ae4d166fbb233719e604dcecc365e9c30ffebf92
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-10-27 13:52:32 +0530

    Refactor Badge component to add custom class and color support

--- src/components/ui/Badge/Badge.js
+++ src/components/ui/Badge/Badge.js
@@ -1,8 +1,20 @@
-const Badge = ({ children, ...props }) => {
-  return <span className='rui-badge'{...props}>
+'use client';
 
-    {children}
-  </span>;
+import {customClassSwitcher} from '@/core';
+
+const COMPONENT_NAME = 'Badge';
+const Badge = ({children, customRootClass = '', className = '', color=undefined, ...props}) => {
+    const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME);
+
+    if (color) {
+        props['data-accent-color'] = color;
+    }
+
+    return <span className={`${rootClass} ${className}`} {...props}>
+        {children}
+    </span>;
 };
+
+Badge.displayName = COMPONENT_NAME;
 
 export default Badge;
