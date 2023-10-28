66f67c73ce5cefc716a8b67f9b87866f52a4b245
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-10-28 08:44:46 +0530

    Add support for "as" prop and reusable function to check and render it

--- src/components/ui/Text/Text.js
+++ src/components/ui/Text/Text.js
@@ -1,6 +1,10 @@
 'use client';
 import {customClassSwitcher} from '@/core';
 
+// Can be rendered as p, label, div, span, etc.
+// TODO: Add as prop support
+// TODO: Add a core reusable function to check and render an as prop
+
 const COMPONENT_NAME = 'Text';
 const Text = ({children, customRootClass = '', className = '', ...props}) => {
     const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME);
