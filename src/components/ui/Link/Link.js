97a5c86428375535e7473643e2af87e7a1c1b29a
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-10-28 13:24:27 +0530

    Refactor Link component to include custom class switching and display name

--- src/components/ui/Link/Link.js
+++ src/components/ui/Link/Link.js
@@ -1,5 +1,14 @@
-const Link = ({children, ...props}) => (
-    <a {...props}>{children}</a>
-);
+'use client';
+import React from 'react';
+
+import {customClassSwitcher} from '@/core';
+
+const COMPONENT_NAME = 'Link';
+const Link = ({children, href='#', alt='', customRootClass = '', className = '', ...props}) => {
+    const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME);
+    return <a href={href} alt={alt} className={`${rootClass} ${className}`} {...props}>{children}</a>;
+};
+
+Link.displayName = COMPONENT_NAME;
 
 export default Link;
