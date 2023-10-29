e74323e6b7d5426532c53d0eaca2f218a663e366
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-10-29 17:11:09 +0530

    Pranay/add colors (#84)

--- src/colors/index.js
+++ src/colors/index.js
@@ -15,6 +15,25 @@ import crimson from './crimson.js';
 import pink from './pink.js';
 import plum from './plum.js';
 import purple from './purple.js';
+import violet from './violet.js';
+
+import iris from './iris.js';
+import indigo from './indigo.js';
+import blue from './blue.js';
+import cyan from './cyan.js';
+import teal from './teal.js';
+import jade from './jade.js';
+import green from './green.js';
+import grass from './grass.js';
+import brown from './brown.js';
+import bronze from './bronze.js';
+import gold from './gold.js';
+import sky from './sky.js';
+import mint from './mint.js';
+import lime from './lime.js';
+import yellow from './yellow.js';
+import amber from './amber.js';
+import orange from './orange.js';
 
 const colors = {
     gray: gray,
@@ -30,6 +49,25 @@ const colors = {
     pink: pink,
     plum: plum,
     purple: purple,
+    violet: violet,
+    iris: iris,
+    indigo: indigo,
+    blue: blue,
+    cyan: cyan,
+    teal: teal,
+    jade: jade,
+    green: green,
+    grass: grass,
+    brown: brown,
+    bronze: bronze,
+    gold: gold,
+    sky: sky,
+    mint: mint,
+    lime: lime,
+    yellow: yellow,
+    amber: amber,
+    orange: orange,
+
 };
 
 export default colors;
