0c1fdd098e83122dcf37d366aaea4c53c0d0382c
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-11-22 14:34:31 +0530

    Add Heading component to Dropdown stories

--- src/components/ui/Dropdown/Dropdown.stories.js
+++ src/components/ui/Dropdown/Dropdown.stories.js
@@ -1,5 +1,5 @@
 
-import {Dropdown} from '@/';
+import {Dropdown, Heading} from '@/';
 import SandboxEditor from '@/components/tools/SandboxEditor/SandboxEditor';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -7,15 +7,13 @@ export default {
     title: 'UI/Input/Dropdown',
     component: Dropdown,
     render: (args) => <SandboxEditor>
-        <div >
-            <div className='flex space-x-2'>
-                Dropdown here
+        <div className=' bg-gray-400 p-4' >
+            <div className='block'>
+                <Heading className="text-gray-1000"> Dropdown here</Heading>
                 <Dropdown onSelect={() => {}} label={'Bello'} options={[
                     {value: 'hello', label: 'hello'},
                 ]}>Hello</Dropdown>
-
             </div>
-
         </div>
     </SandboxEditor>,
 };
