2372d509cfbd95a57325c7a442cd9a035daa56e0
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-12-02 00:25:53 +0530

    Refactor Tooltip component to remove unnecessary code and simplify structure

--- src/components/ui/Tooltip/Tooltip.js
+++ src/components/ui/Tooltip/Tooltip.js
@@ -1,45 +1,11 @@
-import {useState, useRef} from 'react';
-
-import {useFloating, useInteractions, useHover, FloatingArrow, arrow, offset} from '@floating-ui/react';
 
 // TODO : Use Floating Portal?
 
 import Popper from '@/components/tools/Popper/Popper';
 
-const ARROW_HEIGHT = 7;
-const GAP = 2;
-
 const Tooltip = ({children, label}) => {
-    const arrowRef = useRef(null);
-
-    const [isOpen, setIsOpen] = useState(false);
-
-    const {refs, floatingStyles, context} = useFloating({
-        open: isOpen,
-        middleware: [
-            arrow({
-                element: arrowRef,
-            }),
-            offset(ARROW_HEIGHT + GAP),
-
-        ],
-        onOpenChange: setIsOpen,
-        placement: 'top',
-    });
-
-    const hover = useHover(context, {
-        // delay: 400,
-    });
-
-    const {getReferenceProps, getFloatingProps} = useInteractions([
-        hover,
-    ]);
-
     return <div>
-        <div>
-            <Popper pop={'hello'}>{children}</Popper>
-        </div>
-
+        <Popper pop={'hello'}>{children}</Popper>
     </div>;
 };
 
