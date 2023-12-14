6e30706380afea79fb196551a331cd018fddfc68
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-12-14 14:01:45 +0530

    Update Package Json (#127)

--- src/components/ui/Kbd/Kbd.stories.js
+++ src/components/ui/Kbd/Kbd.stories.js
@@ -3,7 +3,7 @@ import SandboxEditor from '@/components/tools/SandboxEditor/SandboxEditor';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-    title: 'UI/Data Display/Kbd',
+    title: 'UI/Typography/Kbd',
     component: Kbd,
     render: (args) => <SandboxEditor>
         <div className='flex space-x-2'>
