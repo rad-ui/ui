5baed45c8dab6d8ab165ad6d7fa362892f33f7e0
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-12-16 09:41:15 +0530

    Add Avatar.dist.stories.js component for UI/Data-Display/Avatar

---
+++ src/components/ui/Avatar/Avatar.dist.stories.js
@@ -0,0 +1,24 @@
+import Avatar from '@radui/ui/dist/ui/Avatar';
+import SandboxEditor from '@/components/tools/SandboxEditor/SandboxEditor';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'UI/Data-Display/Avatar',
+    component: Avatar,
+    render: (args) => <SandboxEditor>
+        <div >
+            <div className='flex space-x-2'>
+                <Avatar fallback="A" size={32} />
+                <Avatar fallback="PK" size={32} />
+                <Avatar src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop" size={32} />
+            </div>
+        </div>
+    </SandboxEditor>,
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const AvatarProd = {
+    args: {
+        className: '',
+    },
+};
