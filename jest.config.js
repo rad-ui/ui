4e51eb71a4c1d1724bd02c94caa61b73a99b613d
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-12-17 13:49:48 +0530

    Enable verbose output, use jsdom test environment, add setupTests.js to setup files after environment, and mock CSS imports

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 /** @type {import('jest').Config} */
 const config = {
-    verbose: false,
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['./src/setupTests.js'],
+    verbose: false, // enable to see the full test suite output, console.log, etc.
+    testEnvironment: 'jsdom', // enable to use DOM APIs
+    setupFilesAfterEnv: ['./src/setupTests.js'], // enable to use custom setup files
     moduleNameMapper: {
-        '\\.(css|less|scss)$': 'identity-obj-proxy',
+        '\\.(css|less|scss)$': 'identity-obj-proxy', // enable to mock CSS imports
     },
 };
 
