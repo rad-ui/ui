44cb9c543c4b8ee48b5e2ebf21b086ea3e503de6
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-12-17 13:49:57 +0530

    Add tests for rendering Avatar component

--- src/components/ui/Avatar/tests/avatar.test.js
+++ src/components/ui/Avatar/tests/avatar.test.js
@@ -1,9 +1,24 @@
 // MyComponent.test.js
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
 import Avatar from '../Avatar';
 
-test('renders MyComponent with message', () => {
-    render(<Avatar fallback="RU" />);
-    expect(screen.getByText('RU')).toBeInTheDocument();
+describe('Avatar', () => {
+    test('renders Avatar component', () => {
+        render(<Avatar fallback="RU" />);
+        expect(screen.getByText('RU')).toBeInTheDocument();
+    });
+
+    test('renders img tag with valid src', () => {
+        const url = 'https://i.pravatar.cc/300';
+        render(<Avatar src={url} fallback="RU" />);
+        const image = screen.getByRole('img');
+        // check if image has url as src
+        expect(image).toHaveAttribute('src', url);
+    });
+
+    test('renders fallback when src is not provided', async() => {
+        render(<Avatar fallback="RU" />);
+        expect(screen.getByText('RU')).toBeInTheDocument();
+    });
 });
