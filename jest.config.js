780ac4bea506c709514eb1adffe0035b4f86c055
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-12-17 13:06:44 +0530

    Add Jest configuration and testing dependencies

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('jest').Config} */
+const config = {
+  verbose: true,
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["./src/setupTests.js"],
+  moduleNameMapper: {
+    "\\.(css|less|scss)$": "identity-obj-proxy",
+  },
+};
+
+module.exports = config;
