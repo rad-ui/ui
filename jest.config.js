29f9b9ab049776a003b3609c1f0b38bbf7163369
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-12-17 13:06:53 +0530

    Update jest.config.js and setupTests.js

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 /** @type {import('jest').Config} */
 const config = {
-  verbose: true,
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["./src/setupTests.js"],
-  moduleNameMapper: {
-    "\\.(css|less|scss)$": "identity-obj-proxy",
-  },
+    verbose: true,
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['./src/setupTests.js'],
+    moduleNameMapper: {
+        '\\.(css|less|scss)$': 'identity-obj-proxy',
+    },
 };
 
 module.exports = config;
