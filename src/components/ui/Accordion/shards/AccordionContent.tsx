227c4157321d170c746e533787489cdf972c1454
Pranay Kothapalli <pranaykothapalli@gmail.com>
2023-12-19 21:35:20 +0530

    add: accordion shards

---
+++ src/components/ui/Accordion/shards/AccordionContent.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+// @ts-ignore
+import {customClassSwitcher} from '~/core';
+
+type AccordionContentProps = {
+  children: React.ReactNode;
+  index: number,
+    activeIndex: number,
+  customRootClass:string
+};
+
+const AccordionContent: React.FC<AccordionContentProps> = ({children, index, activeIndex, customRootClass=''}) => {
+    const rootClass = customClassSwitcher(customRootClass, 'Accordion');
+    return (
+        <span className={`${rootClass}-content`}>
+            <div
+                id={`content-${index}`}
+                role="region"
+                aria-labelledby={`section-${index}`}
+                hidden={activeIndex !== index}
+            >
+                {children}
+            </div>
+
+        </span>
+    );
+};
+
+export default AccordionContent;
+
