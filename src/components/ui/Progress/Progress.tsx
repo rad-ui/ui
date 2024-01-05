abb730c368850d29bc472a503ce26bb9b25b5bbd
Pranay Kothapalli <pranaykothapalli@gmail.com>
2024-01-05 13:37:18 +0530

    Add Progress component with label, value and maxValue props

---
+++ src/components/ui/Progress/Progress.tsx
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react';
+
+interface ProgressProps {
+  value: number;
+  maxValue: number;
+  label: string;
+}
+
+const Progress: React.FC<ProgressProps> = ({value, maxValue, label}) => {
+    console.log(label);
+    const [percentage, setPercentage] = useState(0);
+
+    useEffect(() => {
+        const calculatedPercentage = (value / maxValue) * 100;
+        setPercentage(calculatedPercentage);
+    }, [value, maxValue]);
+
+    return (
+        <div className="bg-gray-300 rounded-md">
+            <div
+                role="progressbar"
+                className='bg-red-800 rounded-md text-right'
+                aria-valuenow={value}
+                aria-valuemax={maxValue}
+                aria-valuemin={0}
+                style={{width: `${percentage}%`}}
+            >
+                <span className='text-gray-1000 p-2 text-sm'>{label}</span>
+            </div>
+        </div>
+    );
+};
+
+export default Progress;
