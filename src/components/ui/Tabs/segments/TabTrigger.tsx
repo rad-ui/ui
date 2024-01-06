32eff0e63eafd6e2391e51589dc2b21c71ce32f2
CCleanerShot <31040856+CCleanerShot@users.noreply.github.com>
2024-01-06 03:51:52 -0600

    migration to typescript (4)

---
+++ src/components/ui/Tabs/segments/TabTrigger.tsx
@@ -0,0 +1,38 @@
+'use client';
+import React from 'react';
+import {customClassSwitcher} from '~/core';
+import { Tab } from '../types';
+
+const COMPONENT_NAME = 'TabTrigger';
+
+export interface TabTriggerProps {
+    tab: Tab;
+    setActiveTab: React.Dispatch<Tab>;
+    activeTab: Tab;
+    className?: string;
+    customRootClass?: string;
+    index: number;
+    props?: Record<string, any>[]
+}
+
+const TabTrigger = ({tab, setActiveTab, activeTab, className, customRootClass, index, ...props}: TabTriggerProps) => {
+    const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME);
+
+    const isActive = activeTab === tab.value;
+
+    const handleClick = (tab: Tab) => {
+        setActiveTab(tab.value);
+    };
+
+    return (
+        <button role="tab" key={index} className={`${rootClass} ${isActive?'active':''} ${className}`} {...props} onClick={() => handleClick(tab)}>
+            <span className={`${rootClass}-inner`}>
+                {tab.label}
+            </span>
+        </button>
+    );
+};
+
+TabTrigger.displayName = 'TabTrigger';
+
+export default TabTrigger;
