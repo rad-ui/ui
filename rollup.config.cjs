c3fc359e4e04bdb4640b3d44440b61fe7bede220
Pranay Kothapalli <pranaykothapalli@gmail.com>
2024-01-06 21:05:23 +0530

    Remove unused import statement

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,14 +1,13 @@
 // rollup.config.js
 import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
-import external from 'rollup-plugin-peer-deps-external';
+
 import terser from '@rollup/plugin-terser';
 import postcss from 'rollup-plugin-postcss';
 import typescript from '@rollup/plugin-typescript';
 import alias from '@rollup/plugin-alias';
 import path from 'path';
 import fs from 'fs';
-
 
 
 import banner2 from 'rollup-plugin-banner2';
