ffe66550844924a2bdf0968038d36c3ca0831663
Pranay Kothapalli <pranaykothapalli@gmail.com>
2024-01-12 16:52:49 +0530

    Add Strong component with props in Strong.tsx

--- src/components/ui/Strong/Strong.tsx
+++ src/components/ui/Strong/Strong.tsx
@@ -9,12 +9,13 @@ export type StrongProps = {
     children: React.ReactNode,
     className?: string,
     customRootClass?: string
+    props?: any
 }
 
-const Strong = ({children, className, customRootClass}: StrongProps) => {
+const Strong = ({children, className, customRootClass, ...props}: StrongProps) => {
     const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME);
     return (
-        <strong className={`${rootClass} ${className}`}>{children}</strong>
+        <strong className={`${rootClass} ${className}`} {...props} >{children}</strong>
     );
 };
 
