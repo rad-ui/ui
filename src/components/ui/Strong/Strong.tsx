63f7d61c9909ffc15362988f57f78281e3722f66
Pranay Kothapalli <pranaykothapalli@gmail.com>
2024-01-12 16:39:10 +0530

    add: Strong component

--- src/components/ui/Strong/Strong.tsx
+++ src/components/ui/Strong/Strong.tsx
@@ -1,13 +1,23 @@
+'use client';
 import React from 'react';
 
-interface StrongProps {
-    children: React.ReactNode
+import {customClassSwitcher} from '~/core';
+const COMPONENT_NAME = 'Strong';
+
+
+export type StrongProps = {
+    children: React.ReactNode,
+    className?: string,
+    customRootClass?: string
 }
 
-const Strong = ({children}: StrongProps) => {
+const Strong = ({children, className, customRootClass}: StrongProps) => {
+    const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME);
     return (
-        <strong>{children}</strong>
+        <strong className={`${rootClass} ${className}`}>{children}</strong>
     );
 };
+
+Strong.displayName = COMPONENT_NAME;
 
 export default Strong;
