e274d678924b223da476953902935a882ff0f00d
corners2wall <akosto4kin@gmail.com>
2024-02-02 18:40:37 +0300

    fix: revert changes

--- src/components/ui/Progress/shards/ProgressRoot.tsx
+++ src/components/ui/Progress/shards/ProgressRoot.tsx
@@ -4,14 +4,8 @@ import {customClassSwitcher} from '~/core';
 
 import {ProgressProps, COMPONENT_NAME} from '../Progress';
 
-// Removed empty interface declaration for clarity
-
 export default function ProgressRoot({children, customRootClass}: ProgressProps) {
     const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME);
 
-    return (
-        <div className={`${rootClass}-root`}>
-            {children}
-        </div>
-    );
+    return (<div className={rootClass}>{children}</div>);
 }
