4dd6fb35ce6dd97964502769e79b8bd74a37bf7e
Pranay Kothapalli <pranaykothapalli@gmail.com>
2024-02-17 15:36:59 +0530

    Refactor ToggleGroupRoot and ToggleItem components

--- src/components/ui/ToggleGroup/shards/ToggleGroupRoot.tsx
+++ src/components/ui/ToggleGroup/shards/ToggleGroupRoot.tsx
@@ -3,23 +3,22 @@ import React, {useState} from 'react';
 
 import {ToggleContext} from '../contexts/toggleContext';
 
-const ToggleGroupRoot = ({type ="multiple", value=null,  children}:any) => {
-
+const ToggleGroupRoot = ({type ='multiple', value=null, children}:any) => {
     // value can be either a string or an array of strings
     // if its null, then no toggles are active
-    
+
     const [activeToggles, setActiveToggles] = useState(value || []);
 
 
     return (
         <div className="toggle-group">
-            <ToggleContext.Provider 
+            <ToggleContext.Provider
                 value={{
-                        activeToggles,
-                        setActiveToggles,
-                        type
+                    activeToggles,
+                    setActiveToggles,
+                    type,
                 }}>
-                    {children}
+                {children}
             </ToggleContext.Provider>
         </div>
     );
--- src/components/ui/ToggleGroup/shards/ToggleItem.tsx
+++ src/components/ui/ToggleGroup/shards/ToggleItem.tsx
@@ -3,13 +3,8 @@ import React, {useContext} from 'react';
 import {ToggleContext} from '../contexts/toggleContext';
 
 
-
 const ToggleItem = ({children, value=null, ...props}:any) => {
     const toggleContext = useContext(ToggleContext);
-
-
-
-
 
 
     const type = toggleContext?.type;
@@ -18,7 +13,7 @@ const ToggleItem = ({children, value=null, ...props}:any) => {
 
     const handleToggleSelect = () => {
         let activeToggleArray = toggleContext?.activeToggles || [];
-    
+
         // For Single Case
         if (type === 'single') {
             if (isActive) {
@@ -29,7 +24,7 @@ const ToggleItem = ({children, value=null, ...props}:any) => {
                 return;
             }
         }
-    
+
         // For Multiple Case
         if (type === 'multiple') {
             if (isActive) {
@@ -38,15 +33,16 @@ const ToggleItem = ({children, value=null, ...props}:any) => {
                 activeToggleArray = [...activeToggleArray, value]; // Using spread operator to create a new array
             }
         }
-    
+
         toggleContext?.setActiveToggles(activeToggleArray);
         console.log(toggleContext?.activeToggles);
     };
-    
 
 
-
-    return <div  className={`${isActive?'bg-blue-600':''}`} onClick={()=>{handleToggleSelect()}}>{children}</div>;
+    return <div
+        className={`${isActive?'bg-blue-600':''}`} onClick={() => {
+            handleToggleSelect();
+        }}>{children}</div>;
 };
 
 export default ToggleItem;
