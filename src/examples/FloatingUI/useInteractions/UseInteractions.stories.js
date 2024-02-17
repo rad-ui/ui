1a8aea8fb3e0613854c38f33116cb61d3a092b1f
Pranay Kothapalli <pranaykothapalli@gmail.com>
2024-02-17 11:04:13 +0530

    Add UseInteractions example with user interactions [WIP] (#223)

---
+++ src/examples/FloatingUI/useInteractions/UseInteractions.stories.js
@@ -0,0 +1,21 @@
+
+import SandboxEditor from '~/components/tools/SandboxEditor/SandboxEditor';
+import UserInteractionsExample from './UseInteractionsExample';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Examples/FloatingUI/UseInteractions',
+    component: 'UseInteractions',
+    render: (args) => <SandboxEditor>
+        <div>
+            <UserInteractionsExample/>
+        </div>
+    </SandboxEditor>,
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default = {
+    args: {
+        className: '',
+    },
+};
