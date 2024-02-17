2b153608894b0eed3f28250f8ad4cc98edbdb521
Pranay Kothapalli <pranaykothapalli@gmail.com>
2024-02-17 15:36:49 +0530

    Add ToggleItem value prop and handle toggle selection

--- src/components/ui/ToggleGroup/ToggleGroup.tsx
+++ src/components/ui/ToggleGroup/ToggleGroup.tsx
@@ -6,11 +6,15 @@ import ToggleItem from './shards/ToggleItem';
 const ToggleGroup = () => {
     return <ToggleGroupRoot>
        this is group
-        <ToggleItem>item 1</ToggleItem>
+        <ToggleItem value="one">item 1</ToggleItem>
+        <ToggleItem value="two">item 2</ToggleItem>
+        <ToggleItem value="three">item 3</ToggleItem>
+        <ToggleItem value="four">item 4</ToggleItem>
     </ToggleGroupRoot>;
 };
 
 
 ToggleGroup.Root = ToggleGroupRoot;
+ToggleGroup.Item = ToggleItem;
 
 export default ToggleGroup;
