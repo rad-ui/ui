92df1e70379cc66ecdfbf4a8fd221af539418ce6
Pranay Kothapalli <pranaykothapalli@gmail.com>
2024-06-26 21:19:21 +0530

    Add single and multiple toggle types to ToggleGroup

--- src/components/ui/ToggleGroup/ToggleGroup.tsx
+++ src/components/ui/ToggleGroup/ToggleGroup.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from 'react'
 
-import ToggleGroupRoot from './shards/ToggleGroupRoot';
-import ToggleItem from './shards/ToggleItem';
+import ToggleGroupRoot from './shards/ToggleGroupRoot'
+import ToggleItem from './shards/ToggleItem'
 
-const ToggleGroup = () => {
-    return <ToggleGroupRoot>
-       this is group
-        <ToggleItem value="one">item 1</ToggleItem>
-        <ToggleItem value="two">item 2</ToggleItem>
-        <ToggleItem value="three">item 3</ToggleItem>
-        <ToggleItem value="four">item 4</ToggleItem>
-    </ToggleGroupRoot>;
-};
+const ToggleGroup = ({ type = 'single' }) => {
+  return (
+    <ToggleGroupRoot type={type}>
+      <ToggleItem value="one">item 1</ToggleItem>
+      <ToggleItem value="two">item 2</ToggleItem>
+      <ToggleItem value="three">item 3</ToggleItem>
+      <ToggleItem value="four">item 4</ToggleItem>
+    </ToggleGroupRoot>
+  )
+}
 
+ToggleGroup.Root = ToggleGroupRoot
+ToggleGroup.Item = ToggleItem
 
-ToggleGroup.Root = ToggleGroupRoot;
-ToggleGroup.Item = ToggleItem;
-
-export default ToggleGroup;
+export default ToggleGroup
--- src/components/ui/ToggleGroup/contexts/toggleContext.tsx
+++ src/components/ui/ToggleGroup/contexts/toggleContext.tsx
@@ -1,5 +1,3 @@
-import {createContext} from 'react';
+import { createContext } from 'react'
 
-export const ToggleContext = createContext({});
-
-
+export const ToggleContext = createContext({})
