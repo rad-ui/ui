3ce79db04d9b7855f11a3f0a1617bc3301c2186c
Vempaati <sriharsha.vempaati@gmail.com>
2024-06-26 13:11:36 +0530

    fixed linter issues part-1

--- src/colors/index.js
+++ src/colors/index.js
@@ -1,68 +1,67 @@
-import gray from './gray.js';
-import mauve from './mauve.js';
-import slate from './slate.js';
-import sage from './sage.js';
-import olive from './olive.js';
-import sand from './sand.js';
-import tomato from './tomato.js';
-import red from './red.js';
-import ruby from './ruby.js';
-import crimson from './crimson.js';
-import pink from './pink.js';
-import plum from './plum.js';
-import purple from './purple.js';
-import violet from './violet.js';
-import iris from './iris.js';
-import indigo from './indigo.js';
-import blue from './blue.js';
-import cyan from './cyan.js';
-import teal from './teal.js';
-import jade from './jade.js';
-import green from './green.js';
-import grass from './grass.js';
-import brown from './brown.js';
-import bronze from './bronze.js';
-import gold from './gold.js';
-import sky from './sky.js';
-import mint from './mint.js';
-import lime from './lime.js';
-import yellow from './yellow.js';
-import amber from './amber.js';
-import orange from './orange.js';
+import gray from './gray'
+import mauve from './mauve'
+import slate from './slate'
+import sage from './sage'
+import olive from './olive'
+import sand from './sand'
+import tomato from './tomato'
+import red from './red'
+import ruby from './ruby'
+import crimson from './crimson'
+import pink from './pink'
+import plum from './plum'
+import purple from './purple'
+import violet from './violet'
+import iris from './iris'
+import indigo from './indigo'
+import blue from './blue'
+import cyan from './cyan'
+import teal from './teal'
+import jade from './jade'
+import green from './green'
+import grass from './grass'
+import brown from './brown'
+import bronze from './bronze'
+import gold from './gold'
+import sky from './sky'
+import mint from './mint'
+import lime from './lime'
+import yellow from './yellow'
+import amber from './amber'
+import orange from './orange'
 
 const colors = {
-    gray: gray,
-    mauve: mauve,
-    slate: slate,
-    sage: sage,
-    olive: olive,
-    sand: sand,
-    tomato: tomato,
-    red: red,
-    ruby: ruby,
-    crimson: crimson,
-    pink: pink,
-    plum: plum,
-    purple: purple,
-    violet: violet,
-    iris: iris,
-    indigo: indigo,
-    blue: blue,
-    cyan: cyan,
-    teal: teal,
-    jade: jade,
-    green: green,
-    grass: grass,
-    brown: brown,
-    bronze: bronze,
-    gold: gold,
-    sky: sky,
-    mint: mint,
-    lime: lime,
-    yellow: yellow,
-    amber: amber,
-    orange: orange,
+  gray,
+  mauve,
+  slate,
+  sage,
+  olive,
+  sand,
+  tomato,
+  red,
+  ruby,
+  crimson,
+  pink,
+  plum,
+  purple,
+  violet,
+  iris,
+  indigo,
+  blue,
+  cyan,
+  teal,
+  jade,
+  green,
+  grass,
+  brown,
+  bronze,
+  gold,
+  sky,
+  mint,
+  lime,
+  yellow,
+  amber,
+  orange
+}
 
-};
-
-export default colors;
+export default colors
--- src/core/customClassSwitcher.ts
+++ src/core/customClassSwitcher.ts
@@ -1,13 +1,18 @@
-const RAD_UI_CLASS_PREFIX = 'rad-ui';
+const RAD_UI_CLASS_PREFIX = 'rad-ui'
 
-export const customClassSwitcher = (customRootClass: string = '', componentName: string = ''): string => {
-    // applies a custom root class the user provides, else applies the default rad-ui classes to the component
-    // rad ui's classes are based on this logic
+export const customClassSwitcher = (
+  customRootClass: string = '',
+  componentName: string = ''
+): string => {
+  // applies a custom root class the user provides, else applies the default rad-ui classes to the component
+  // rad ui's classes are based on this logic
 
-    // add dashes between capitalized words
-    const componentClassName = componentName.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
-    if (customRootClass) return customRootClass;
-    if (componentName) return `${RAD_UI_CLASS_PREFIX}-${componentClassName}`;
+  // add dashes between capitalized words
+  const componentClassName = componentName
+    .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
+    .toLowerCase()
+  if (customRootClass) return customRootClass
+  if (componentName) return `${RAD_UI_CLASS_PREFIX}-${componentClassName}`
 
-    return '';
-};
+  return ''
+}
