898350651faaa97bfb69613e6be3d766f94b4a45
Pranay Kothapalli <pranaykothapalli@gmail.com>
2024-06-26 21:18:07 +0530

    Revert "Part 1: fixed linter issues (#339)"
    This reverts commit 8b35885d88e38d281c1954a3379f106b1308d288.

--- src/examples/Colors/Colors.stories.js
+++ src/examples/Colors/Colors.stories.js
@@ -1,37 +1,41 @@
-import React, { useState } from 'react'
 
-import colors from '../../colors/index'
+import React, {useState} from 'react';
 
-import ColorsTemplate from './ColorsTemplate'
+import colors from '../../colors/index';
 
-function AllColorsTemplate() {
-  const [darkMode, setDarkMode] = useState(false)
+import ColorsTemplate from './ColorsTemplate';
 
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode)
-  }
-  return (
-    <div className={darkMode ? 'rad-ui-dark-theme' : ''}>
-      <button className="text-gray-900" onClick={toggleDarkMode}>
-        {darkMode ? 'Go To Light Mode' : 'Go To Dark Mode'}
-      </button>
-      <div className={`flex ${darkMode ? 'bg-black' : ''} `}>
-        <div isDark={darkMode}>
-          <ColorsTemplate isDark={darkMode} />
+const AllColorsTemplate = () => {
+    const [darkMode, setDarkMode] = useState(false);
+
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+    };
+    return (
+        <div className={darkMode?'rad-ui-dark-theme':''}>
+            <button className='text-gray-900' onClick={toggleDarkMode}>
+                {darkMode ? 'Go To Light Mode' : 'Go To Dark Mode'}
+            </button>
+            <div className={`flex ${darkMode?'bg-black':''} `}>
+
+                <div isDark={darkMode}>
+                    <ColorsTemplate isDark={darkMode}/>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
-}
+
+    );
+};
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Colors',
-  component: 'AllColorsTemplate',
-  render: () => <AllColorsTemplate />
-}
+    title: 'Example/Colors',
+    component: 'AllColorsTemplate',
+    render: (args) => <AllColorsTemplate/>,
+};
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const All = {
-  args: {}
-}
+    args: {
+    },
+};
--- src/examples/FloatingUI/useInteractions/UseInteractions.stories.js
+++ src/examples/FloatingUI/useInteractions/UseInteractions.stories.js
@@ -1,22 +1,21 @@
-import SandboxEditor from '~/components/tools/SandboxEditor/SandboxEditor'
-import UserInteractionsExample from './UseInteractionsExample'
+
+import SandboxEditor from '~/components/tools/SandboxEditor/SandboxEditor';
+import UserInteractionsExample from './UseInteractionsExample';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Examples/FloatingUI/UseInteractions',
-  component: 'UseInteractions',
-  render: () => (
-    <SandboxEditor>
-      <div>
-        <UserInteractionsExample />
-      </div>
-    </SandboxEditor>
-  )
-}
+    title: 'Examples/FloatingUI/UseInteractions',
+    component: 'UseInteractions',
+    render: (args) => <SandboxEditor>
+        <div>
+            <UserInteractionsExample/>
+        </div>
+    </SandboxEditor>,
+};
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Default = {
-  args: {
-    className: ''
-  }
-}
+    args: {
+        className: '',
+    },
+};
