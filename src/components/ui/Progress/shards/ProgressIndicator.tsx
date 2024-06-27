0d49f885fec346e10cf058b3c692837bcb53f345
Pranay Kothapalli <pranaykothapalli@gmail.com>
2024-06-28 02:12:17 +0530

    Revert eslint config (#354)

--- src/components/ui/Progress/shards/ProgressIndicator.tsx
+++ src/components/ui/Progress/shards/ProgressIndicator.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
-import { ProgressProps, COMPONENT_NAME } from '../Progress'
-import { customClassSwitcher } from '~/core'
+import React, {useEffect} from 'react';
+import {ProgressProps, COMPONENT_NAME} from '../Progress';
+import {customClassSwitcher} from '~/core';
 
 interface IndicatorProps
   extends Pick<
@@ -11,30 +11,30 @@ interface IndicatorProps
 }
 
 export default function ProgressIndicator({
-  value,
-  minValue = 0,
-  maxValue = 100,
-  customRootClass,
-  renderLabel
+    value,
+    minValue = 0,
+    maxValue = 100,
+    customRootClass,
+    renderLabel,
 }: IndicatorProps) {
-  const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME)
+    const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME);
 
-  if (value < minValue || value > maxValue) {
-    throw new Error(
-      `value should be greater than or equal to ${minValue} and less than or equal to ${maxValue}`
-    )
-  }
+    if (value < minValue || value > maxValue) {
+        throw new Error(
+            `value should be greater than or equal to ${minValue} and less than or equal to ${maxValue}`,
+        );
+    }
 
-  return (
-    <div
-      role="progressbar"
-      className={`${rootClass}-indicator`}
-      style={{ transform: `translateX(-${maxValue - value}%)` }}
-      aria-valuenow={value}
-      aria-valuemax={maxValue}
-      aria-valuemin={minValue}
-    >
-      {renderLabel && renderLabel(value)}
-    </div>
-  )
+    return (
+        <div
+            role="progressbar"
+            className={`${rootClass}-indicator`}
+            style={{transform: `translateX(-${maxValue - value}%)`}}
+            aria-valuenow={value}
+            aria-valuemax={maxValue}
+            aria-valuemin={minValue}
+        >
+            {renderLabel && renderLabel(value)}
+        </div>
+    );
 }
