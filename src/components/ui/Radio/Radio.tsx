7c0916596158742dd989266911298e0262e4e970
jyoti <jindaljyoti913@gmail.com>
2024-09-20 19:28:20 -0700

    create radio component

---
+++ src/components/ui/Radio/Radio.tsx
@@ -0,0 +1,16 @@
+'use_client';
+import React,{useState} from 'react';
+import { customClassSwitcher} from "~/core";
+const COMPONENT_NAME = 'Radio';
+
+export type RadioProps = {
+color: string;
+    children?: React.ReactNode;
+    className?: string;
+    customRootClass?: string;
+}
+const Radio ={
+    
+}
+
+export default Radio;
