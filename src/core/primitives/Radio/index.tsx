e4480e64d04b040958198e0d318af19fe84a4674
jyoti <jindaljyoti913@gmail.com>
2024-10-12 16:14:41 -0700

    update the index file

--- src/core/primitives/Radio/index.tsx
+++ src/core/primitives/Radio/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const RadioPrimitive = ({role='radio', label='', id, checked, children, ...props}:any) => {
+const RadioPrimitive = ({role='radio', label='', id, value, defaultValue, onValueChanged, checked, children, ...props}:any) => {
        if(label){
 
         props['aria-label'] = label     
@@ -15,7 +15,11 @@ const RadioPrimitive = ({role='radio', label='', id, checked, children, ...props
 
        return (
        <div>
-           <input type="radio" role={role} id={id} {...props} checked={!checked}/>
+        
+          
+          <input type='radio' id={id} {...props} value={value} checked={defaultValue} onValueChanged={onValueChanged} />
+             <button type="button" role={role}>
+           </button>
            <label htmlFor={id}> {children}</label>
           
        </div>
