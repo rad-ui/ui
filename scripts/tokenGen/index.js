405b83547fb23edd2770bcef48fbf564fc0368af
Pranay Kothapalli <pranaykothapalli@gmail.com>
2024-10-26 22:19:01 +0530

    Add target branch for Dependabot weekly updates (#479)

--- scripts/tokenGen/index.js
+++ scripts/tokenGen/index.js
@@ -1,43 +1,44 @@
 import fs from 'fs';
+import path from 'path';
 
 import generateCSSTokens from './generateCSSTokens.js';
 import generateAccentTokens from './generateAccentTokens.js';
 
-// Generate CSS variables for the light theme
+try {
+    // Generate CSS variables for the light theme
+    let jsVariables = {};
+    const { cssVariables: lightThemeCss, jsVariables: lightThemeJSTokens } = generateCSSTokens('light');
+    jsVariables = { ...jsVariables, ...lightThemeJSTokens };
 
-let jsVariables = {};
+    // Generate CSS variables for the dark theme
+    const { cssVariables: darkThemeCss, jsVariables: darkThemeJSTokens } = generateCSSTokens('dark');
+    jsVariables = { ...jsVariables, ...darkThemeJSTokens };
 
-const { cssVariables: lightThemeCss, jsVariables: lightThemeJSTokens } = generateCSSTokens('light');
-jsVariables = { ...jsVariables, ...lightThemeJSTokens };
+    // Generate accent colors css
+    const accentTokens = generateAccentTokens('light');
 
-// Generate CSS variables for the dark theme
-const { cssVariables: DarkThemeCss, jsVariables: DarkThemeJSTokens } = generateCSSTokens('dark');
-jsVariables = { ...jsVariables, ...DarkThemeJSTokens };
+    // Combine light and dark theme CSS
+    const combinedCss = `
+    :root {
+      ${lightThemeCss}
+    }
 
-// Generate accent colors css
+    ${accentTokens}
 
-const accentTokens = generateAccentTokens('light');
+    .rad-ui-dark-theme {
+      ${darkThemeCss}
+    }
+  `;
 
-// Combine light and dark theme CSS
-const combinedCss = `
-  :root {
-    ${lightThemeCss}
-   
-  }
+    // Save the CSS to a file
+    const cssFilePath = path.resolve('styles', 'cssTokens', 'base.tokens.css');
+    fs.writeFileSync(cssFilePath, combinedCss, 'utf-8');
+    console.log('CSS file saved as base.tokens.css');
 
-  ${accentTokens}
-
-  .rad-ui-dark-theme {
-    ${DarkThemeCss}
-  }
-`;
-
-// Save the CSS to a file
-fs.writeFileSync('styles/cssTokens/base.tokens.css', combinedCss, 'utf-8');
-console.log('CSS file saved as base.tokens.css');
-
-// write the JS variables to a js file
-// format json
-jsVariables = JSON.stringify(jsVariables);
-fs.writeFileSync('styles/jsTokens/colors.tokens.js', `export default ${jsVariables}`, 'utf-8');
-console.log('JS file saved as colors.tokens.js');
+    // Write the JS variables to a js file
+    const jsFilePath = path.resolve('styles', 'jsTokens', 'colors.tokens.js');
+    fs.writeFileSync(jsFilePath, `export default ${JSON.stringify(jsVariables, null, 2)}`, 'utf-8');
+    console.log('JS file saved as colors.tokens.js');
+} catch (error) {
+    console.error('An error occurred:', error);
+}
