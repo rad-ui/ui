6ecde9ef138420acf7f82c8f493bc060e80bc3de
jyoti <jindaljyoti913@gmail.com>
2024-10-26 11:32:51 -0700

    update the aria props

--- src/core/primitives/Radio/index.tsx
+++ src/core/primitives/Radio/index.tsx
@@ -1,23 +1,16 @@
 import React from "react";
 
 const RadioPrimitive = ({role='radio', label='', id, value, defaultValue, onValueChanged, checked, children, ...props}:any) => {
-       if(label){
-
-        props['aria-label'] = label     
-       }
-
-       if(checked){
-         props['aria-checked'] = 'true'
-       }
-       else {
-          props['aria-checked'] = 'false'
-       }
+      const ariaProps = {
+         'aria-label': label || undefined,
+         'aria-checked': checked ? 'true' : 'false',
+      }
 
        return (
        <div>
         
           
-          <input type='radio' id={id} {...props} value={value} checked={defaultValue} onValueChanged={onValueChanged} />
+          <input type='radio' id={id} {...ariaProps} {...props} value={value} checked={defaultValue} onValueChanged={onValueChanged} />
              <button type="button" role={role}>
            </button>
            <label htmlFor={id}> {children}</label>
