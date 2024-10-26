578e69a964e6dca8ca7e0da2b3b429af50b49d06
jyoti <jindaljyoti913@gmail.com>
2024-10-26 11:56:15 -0700

    add data checked attribute

--- src/core/primitives/Radio/index.tsx
+++ src/core/primitives/Radio/index.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-const RadioPrimitive = ({role='radio', label='', id, value, defaultValue, onValueChanged, checked, children, ...props}:any) => {
+const RadioPrimitive = ({role='radio', label='', id, value, defaultChecked, onChange, checked, children, ...props}:any) => {
       const ariaProps = {
          'aria-label': label || undefined,
          'aria-checked': checked ? 'true' : 'false',
+         'data-checked': checked ? 'true' : 'false'
       }
 
        return (
        <div>
         
           
-          <input type='radio' id={id} {...ariaProps} {...props} value={value} checked={defaultValue} onValueChanged={onValueChanged} />
-             <button type="button" role={role}>
-           </button>
+          <input type='radio' id={id} {...ariaProps} {...props} value={value} checked={defaultChecked} onChange={onChange} />
+             
            <label htmlFor={id}> {children}</label>
           
        </div>
