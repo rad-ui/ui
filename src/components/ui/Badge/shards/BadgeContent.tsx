926ab930b0bd7259cc219541542a8bc92b586ff6
Pranay Kothapalli <pranaykothapalli@gmail.com>
2024-10-27 12:02:36 +0530

    fix: linting

--- src/components/ui/Badge/shards/BadgeContent.tsx
+++ src/components/ui/Badge/shards/BadgeContent.tsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 
 type BadgeContentProps = {
    children: React.ReactNode;
-   
+
 }
 
-const BadgeContent = ({children}:BadgeContentProps) => {
-         return (
-            <>
-              {children}
-            </>
-         )
-}
+const BadgeContent = ({ children }:BadgeContentProps) => {
+    return (
+        <>
+            {children}
+        </>
+    );
+};
 
 export default BadgeContent;
