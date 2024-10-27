d750dc7b5a2c233a9f3ebf0362cc9948c36fdc95
jindaljyoti <137828895+jindaljyoti@users.noreply.github.com>
2024-10-26 20:36:11 -0700

    Refactor badge (#454)

---
+++ src/components/ui/Badge/shards/BadgeRoot.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { customClassSwitcher } from "~/core";
+
+const COMPONENT_NAME = 'Badge';
+
+type BadgeRootProps = {
+     children: React.ReactNode;
+     customRootClass?: string;
+     color?: string;
+     className?: string;
+     props?: Record<string, any>[]
+}
+
+const BadgeRoot = ({children,customRootClass,className,color,...props}:BadgeRootProps) => {
+    const rootClass = customClassSwitcher(customRootClass,COMPONENT_NAME);
+
+        return (
+            <span className= {`${rootClass} ${className}`} data-accent-color={color ?? undefined} {...props}>
+                {children}
+            </span>
+        )
+}
+
+BadgeRoot.displayName = COMPONENT_NAME;
+
+export default BadgeRoot;
