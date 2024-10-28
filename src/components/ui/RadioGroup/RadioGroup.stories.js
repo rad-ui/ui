ec1af483c38e13b1b8dd969596b29e5dc39693ec
jyoti <jindaljyoti913@gmail.com>
2024-10-27 17:56:18 -0700

    move the data state into storybook file

--- src/components/ui/RadioGroup/RadioGroup.stories.js
+++ src/components/ui/RadioGroup/RadioGroup.stories.js
@@ -1,15 +1,31 @@
 import RadioGroup from './RadioGroup';
 import SandboxEditor from '~/components/tools/SandboxEditor/SandboxEditor';
+import { useState } from 'react';
 
 const RadioButton = (args) => {
-   
+   const options = [
+    {id: 'html', value: 'html', label:'HTML'},
+    {id: 'css', value: 'css', label:'CSS'},
+    {id: 'javascript', value: 'javascript', label:'JavaScript'},]
+
+    const [language, setLanguage] = useState({})
+
+    const handleChange = (e) => {
+          setLanguage(e.target.value)
+    }
       return (
         <SandboxEditor>
-              
-            <RadioGroup className='radioItems'{...args}>
-                  
+                {options.map((option) => (
+            <RadioGroup className='radioItems'{...args}
+            key={option.id} 
+            id={option.id}
+            name='language'
+            value={option.value}
+            checked={language === option.value}
+            onChange={handleChange} >
+                <span id={option.id} >{option.label}</span>    
                    </RadioGroup>
-                  
+                    ))}
         </SandboxEditor>
       )
 }
--- src/components/ui/RadioGroup/RadioGroup.tsx
+++ src/components/ui/RadioGroup/RadioGroup.tsx
@@ -1,51 +1,27 @@
-import React, { DetailedHTMLProps, InputHTMLAttributes, PropsWithChildren, useState} from 'react';
+import React, { DetailedHTMLProps, InputHTMLAttributes, PropsWithChildren} from 'react';
 import { customClassSwitcher} from "~/core";
 import RadioPrimitive from '~/core/primitives/Radio';
 const COMPONENT_NAME = 'RadioGroup';
 
-
-type Option = {
-  id: string;
-  value: string;
-  label: string
-}
 export type RadioGroupProps = {
   
     children?: React.ReactNode;
     className: string;
-    customRootClass: string;   
+    customRootClass: string;  
 } & DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & PropsWithChildren
 
-const RadioGroup =({children,type='radio', options = [
-     {id: 'html', value: 'html', label:'HTML'},
-     {id: 'css', value: 'css', label:'CSS'},
-     {id: 'javascript', value: 'javascript', label:'JavaScript'},],
-     className='',customRootClass='', ...props}:RadioGroupProps & {options?: Option[]}) => {
+const RadioGroup =({children,type='radio', className='',customRootClass='', ...props}:RadioGroupProps) => {
     const rootClass= customClassSwitcher(customRootClass,COMPONENT_NAME)
-    const [language, setLanguage] = useState({})
-
-    const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-          setLanguage(e.target.value)
-    }
+   
     return (
       <div className={`${rootClass} ${className}`} role='radiogroup'>
-        {options.map((option) => (
-      <RadioPrimitive 
-         key={option.id}
-         type={type} 
-         id={option.id}
-         name='language'
-         value={option.value}
-         checked={language === option.value}
-         onChange={handleChange}
+        <RadioPrimitive 
+         type={type}
          {...props}>
 
          {children}
-       <span id={option.id} >{option.label}</span>
-      </RadioPrimitive>
-      
-     ))}
-      </div>
+       </RadioPrimitive>
+       </div>
     )
 }
       
