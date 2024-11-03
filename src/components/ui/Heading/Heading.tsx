29992c09752bd57ee7921c571db0998ff62cedb6
sameer2748 <mrao27488@gmail.com>
2024-11-03 21:37:31 +0530

    added type checking in Heading component with the find call to run once

--- src/components/ui/Heading/Heading.tsx
+++ src/components/ui/Heading/Heading.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 import { customClassSwitcher } from '~/core';
 
@@ -29,15 +29,19 @@ const RENDER_AS_ENUMS = [
         tag: 'h6'
     }
 ];
+interface HeadingProps {
+    children: ReactNode;
+    as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+    customRootClass?: string;
+    className?: string;
+    [key: string]: any; // To allow any other props (optional)
+}
 
-const Heading = ({ children, as = undefined, customRootClass = '', className = '' , ...props}) => {
-    const rootClass = customClassSwitcher(customRootClass, as || 'h1');
+const Heading: React.FC<HeadingProps> = ({children, as = 'h1', customRootClass = '', className = '', ...props }) => {
+    const rootClass = customClassSwitcher(customRootClass, as);
+    const Tag = RENDER_AS_ENUMS.find((item) => item.tag === as)?.tag || 'h1';
 
-    if (as !== undefined && RENDER_AS_ENUMS.find((item) => item.tag === as)) {
-        const { tag: Tag } = RENDER_AS_ENUMS.find((item) => item.tag === as);
-        return <Tag className={`${rootClass} ${className}`} {...props}>{children}</Tag>;
-    }
-    return <h1 className={`${rootClass} ${className}`} {...props}>{children}</h1>;
+    return <Tag className={`${rootClass} ${className}`} {...props}>{children}</Tag>;
 };
 Heading.displayName = 'Heading';
 
