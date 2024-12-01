53ae45580575b1504bedd0e3b7f29c86136eae53
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com>
2024-12-02 00:09:00 +0530

    Progress value fixed (#591)

--- src/components/ui/Progress/stories/Progress.stories.js
+++ src/components/ui/Progress/stories/Progress.stories.js
@@ -7,23 +7,17 @@ export default {
     title: 'Components/Progress',
     component: Progress,
     render: (args) => {
-        const [value, setValue] = useState(getRandomValue());
-
-        useEffect(() => {
-            const intervalId = setInterval(() => setValue(getRandomValue()), 2345);
-
-            return () => clearInterval(intervalId);
-        });
+        
 
         return (<SandboxEditor>
             <div className='my-10'>
-                <Progress value={value}/>
+                <Progress {...args}/>
             </div>
         </SandboxEditor>);
     }
 };
 
-const getRandomValue = () => Math.trunc((Math.random() * 100));
+
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const All = {
     args: {
