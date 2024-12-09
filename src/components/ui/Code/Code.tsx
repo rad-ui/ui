7dcdcec547867a3d16ad8e1b5781e9e1f96afe9d
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com>
2024-12-09 16:15:18 +0530

    Util classname 1 (#616)
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/components/ui/Code/Code.tsx
+++ src/components/ui/Code/Code.tsx
@@ -1,12 +1,12 @@
 'use client';
 import React from 'react';
-
+import { clsx } from 'clsx';
 export type CodeProps= {
     children: React.ReactNode;
 }
 
 const Code = ({ children }: CodeProps) => {
-    return <code className='rui-code-root'>
+    return <code className={clsx('rui-code-root')}>
         {children}
     </code>;
 };
