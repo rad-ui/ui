eca794fe65a47a7a3f8310fceb3e9d6d8126d9e1
foobar <uagnl25x@anonaddy.me>
2024-12-11 23:58:55 +0530

    exported dropdown component and prop types

--- src/components/ui/Dropdown/Dropdown.tsx
+++ src/components/ui/Dropdown/Dropdown.tsx
@@ -9,5 +9,26 @@ import {Root, DropdownRootProps} from './shards/DropdownRoot';
  * Add aria-control
  * */
 
-export default {Trigger, Content, Root};
+const COMPONENT_NAME = 'DropdownMenu';
+
 type DropdownMenuProps = PropsWithChildren
+
+const DropdownMenu = ({children}: DropdownMenuProps) => {
+    return (
+        <Root>
+            <Trigger/>
+            <Content >
+                {children}
+            </Content>
+        </Root>
+    );
+};
+
+DropdownMenu.displayName = COMPONENT_NAME;
+DropdownMenu.Root= Root;
+DropdownMenu.Trigger= Trigger;
+DropdownMenu.Content= Content;
+
+export default DropdownMenu;
+
+export type {DropdownMenuProps, DropdownRootProps, DropdownTriggerProps, DropdownContentProps};
