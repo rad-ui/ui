21e1d2fdbb2dc5adcf5833423c9f910536b7bb55
foobar <uagnl25x@anonaddy.me>
2024-12-14 13:48:39 +0530

    test will created

---
+++ src/components/ui/Dropdown/tests/Dropdown.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Dropdown from '../Dropdown';
+
+// const placeholderWords= ['Eternity', 'Wisdom', 'Truth', 'Love', 'Freedom', 'Serenity', 'Hope', 'Courage', 'Grace', 'Harmony', 'Solitude', 'Enlightenment', 'Peace', 'Joy', 'Unity', 'Transcendence', 'Mystery', 'Compassion', 'Faith', 'Destiny'].map((v) => (<div key={v}>{v}</div>));
+
+describe('Avatar', () => {
+    // test('renders Avatar component', () => {
+        // render(<Dropdown>{placeholderWords}</Dropdown>)
+        // expect(screen.getByText('Trigger')).toBeInTheDocument();
+    // });
+});
