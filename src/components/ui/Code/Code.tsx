a0efb52b9d16368c556b982226e7de2f6280e5bb
jindaljyoti <137828895+jindaljyoti@users.noreply.github.com>
2024-12-14 18:31:37 -0800

    Missing customRootClass issue #619 #620 (#648)

--- src/components/ui/Code/Code.tsx
+++ src/components/ui/Code/Code.tsx
@@ -1,12 +1,19 @@
 'use client';
 import React from 'react';
 import { clsx } from 'clsx';
+import { customClassSwitcher } from '~/core';
+
+const COMPONENT_NAME = 'Code';
+
 export type CodeProps= {
     children: React.ReactNode;
+    customRootClass?: string;
 }
 
-const Code = ({ children }: CodeProps) => {
-    return <code className={clsx('rui-code-root')}>
+const Code = ({ children, customRootClass='' }: CodeProps) => {
+      const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME)
+
+    return <code className={clsx(rootClass)}>
         {children}
     </code>;
 };
