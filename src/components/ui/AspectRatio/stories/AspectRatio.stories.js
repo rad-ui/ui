a4afff1a932106f70b954e829e14ab1bb295be01
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com>
2024-12-20 15:27:56 +0530

    Storybook missing styles in aspect ratio example (#660)

--- src/components/ui/AspectRatio/stories/AspectRatio.stories.js
+++ src/components/ui/AspectRatio/stories/AspectRatio.stories.js
@@ -8,6 +8,7 @@ export default {
     render: (args) => <SandboxEditor>
         <AspectRatio {...args} >
             <img
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
 				className="Image"
 				src="https://images.pexels.com/photos/346529/pexels-photo-346529.jpeg?cs=srgb&dl=pexels-bri-schneiter-28802-346529.jpg&fm=jpg"
 				alt="Landscape photograph"
