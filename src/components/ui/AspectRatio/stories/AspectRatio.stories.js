f638e3653b362827d4267852d5d8c9db4f53e1cc
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com>
2024-12-20 14:36:02 +0530

    [New Component] Aspect Ratio component (#656)

---
+++ src/components/ui/AspectRatio/stories/AspectRatio.stories.js
@@ -0,0 +1,25 @@
+import AspectRatio from '../AspectRatio';
+import SandboxEditor from '~/components/tools/SandboxEditor/SandboxEditor';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Components/AspectRatio',
+    component: AspectRatio,
+    render: (args) => <SandboxEditor>
+        <AspectRatio {...args} >
+            <img
+				className="Image"
+				src="https://images.pexels.com/photos/346529/pexels-photo-346529.jpeg?cs=srgb&dl=pexels-bri-schneiter-28802-346529.jpg&fm=jpg"
+				alt="Landscape photograph"
+			/>
+            </AspectRatio>
+    </SandboxEditor>
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default = {
+    args: {
+        className:"",
+        ratio: "16/9"
+    }
+};
