72532f6a36d4ac114b04ba05253a51ee103df902
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com.>
2024-12-20 14:29:04 +0530

    aspect ratio component

--- src/components/ui/AspectRatio/tests/AspectRatio.test.js
+++ src/components/ui/AspectRatio/tests/AspectRatio.test.js
@@ -16,7 +16,28 @@ describe('AspectRatio', () => {
 
     test('applies correct aspect ratio', () => {
         render(<AspectRatio ratio="16/9">Content</AspectRatio>);
-        expect(screen.getByText('Content')).toHaveStyle( "aspect-ratio: 16/9");
+        expect(screen.getByText('Content').style.aspectRatio).toBe('16/9');
     });
+
+    test('applies correct aspect ratio when ratio is not provided', () => {
+        render(<AspectRatio >Content</AspectRatio>);
+        expect(screen.getByText('Content').style.aspectRatio).toBe('1');
+    });
+
+    test('applies correct aspect ratio when ratio is invalid', () => {
+        render(<AspectRatio ratio="invalid">Content</AspectRatio>);
+        expect(screen.getByText('Content').style.aspectRatio).toBe('1');
+    });
+
+    test('applies correct aspect ratio when ratio contains a character', () => {
+        render(<AspectRatio ratio="16/o">Content</AspectRatio>);
+        expect(screen.getByText('Content').style.aspectRatio).toBe('1');
+    });
+
+    test('applies correct aspect ratio when ratio is negative', () => {
+        render(<AspectRatio ratio="-5">Content</AspectRatio>);
+        expect(screen.getByText('Content').style.aspectRatio).toBe('1');
+    });
+
     
 });
