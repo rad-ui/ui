0834f6bee2590d3fba13996c1438903d44b39dd3
Pranay Kothapalli <pranaykothapalli@gmail.com>
2024-12-29 11:43:44 +0530

    Refactor path concatenation to use template literals

--- docs/utils/parseSourceCode.tsx
+++ docs/utils/parseSourceCode.tsx
@@ -6,7 +6,7 @@ export const getComponentScssSourceCode = (componentName: string) => {
     // This function is only meant to be used for scss files inside the styles/themes/components folder
     try {   
         const component_name = componentName.toLowerCase();
-        const path_to_scss = path.join(process.cwd(), '../styles/themes/components/' + component_name + '.scss');
+        const path_to_scss = path.join(process.cwd(), 'styles/themes/components', `${component_name}.scss`);
         const scss_SourceCode = fs.readFileSync(
         path_to_scss,
         'utf8'
@@ -20,7 +20,7 @@ export const getComponentScssSourceCode = (componentName: string) => {
 
 export const getComponentJsxSourceCode = (componentPath: string) => {
     // This function is only meant to be used for jsx files inside the app/docs/components folder
-    const path_to_jsx = path.join(process.cwd(), '/app/docs/components/' + componentPath);
+    const path_to_jsx = path.join(process.cwd(), 'app/docs/components', componentPath);
     const jsx_SourceCode = fs.readFileSync(
         path_to_jsx,
         'utf8'
