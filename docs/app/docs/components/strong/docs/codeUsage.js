5d2dc705254170e82651b466e55ecdcb4364c383
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com.>
2024-12-29 13:39:58 +0530

    Strong Docs

---
+++ docs/app/docs/components/strong/docs/codeUsage.js
@@ -0,0 +1,13 @@
+const code = {
+    javascript: {
+        code: `import Strong from '@radui/ui/Strong'
+
+const StrongExample = () => (
+    <div className='mt-5'>
+            This is a very <Strong className='text-gray-1000'>Strong</Strong> word
+    </div>
+)`
+    }
+}
+
+export default code;
--- docs/components/navigation/Navigation.js
+++ docs/components/navigation/Navigation.js
@@ -105,6 +105,10 @@ const sections = [
                 title: "Switch",
                 path: "/docs/components/switch"
             },
+            {
+                title: "Strong",
+                path: "/docs/components/strong"
+            },
             // {
             //     title:"Checkbox",
             //     path:"/docs/components/checkbox"
