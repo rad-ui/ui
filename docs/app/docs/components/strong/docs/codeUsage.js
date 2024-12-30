3f61bd1791a6909d959d0222782e688a36261de6
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com>
2024-12-30 08:39:43 +0530

    Strong docs fix (#705)

--- docs/app/docs/components/strong/docs/codeUsage.js
+++ docs/app/docs/components/strong/docs/codeUsage.js
@@ -3,8 +3,8 @@ const code = {
         code: `import Strong from '@radui/ui/Strong'
 
 const StrongExample = () => (
-    <div className='mt-5'>
-            This is a very <Strong className='text-gray-1000'>Strong</Strong> word
+    <div>
+            This is a very <Strong>Strong</Strong> word
     </div>
 )`
     }
