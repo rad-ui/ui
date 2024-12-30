f4b82828a5269dee9786042ec33fa589ad8d2bff
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com.>
2024-12-30 15:22:14 +0530

    AvatarGroup Tests

--- src/components/ui/AvatarGroup/tests/Avatar.test.js
+++ src/components/ui/AvatarGroup/tests/Avatar.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import AvatarGroup from '../AvatarGroup';
 
 describe('AvatarGroup', () => {
@@ -21,30 +21,52 @@ describe('AvatarGroup', () => {
     });
 
     test('AvatarGroup renders fallback text when src is not provided', () => {
-        
-        render(<AvatarGroup avatars={avatarsWithFallback} size="md" />);
+        render(<AvatarGroup avatars={avatarsWithFallback} />);
         expect(screen.getByText('A')).toBeInTheDocument();
         expect(screen.getByText('B')).toBeInTheDocument();
     });
 
     test('AvatarGroup applies className correctly', () => {
         const customClass = 'custom-class';
-        render(<AvatarGroup avatars={avatarsWithFallback} size="md" className={customClass} />);
+        render(<AvatarGroup avatars={avatarsWithFallback} className={customClass} />);
         expect(screen.getByText('A').parentElement.parentElement).toHaveClass(customClass);
     });
 
     test('AvatarGroup renders correct number of avatars', () => {
-        render(<AvatarGroup avatars={avatars} size="md" />);
+        render(<AvatarGroup avatars={avatars} />);
         const avatarImages = screen.getAllByRole('img');
         expect(avatarImages.length).toBe(avatars.length);
     });
     
     test('AvatarGroup renders correct src for each avatar', () => {
-        render(<AvatarGroup avatars={avatars} size="md" />);
+        render(<AvatarGroup avatars={avatars} />);
         avatars.forEach(avatar => {
             const img = screen.getByAltText(avatar.alt);
             expect(img).toHaveAttribute('src', avatar.src);
         });
     });
+
+    test('AvatarGroup renders correct src for with fallback as boolean', () => {
+        const brokenFallBack = [
+            { fallback: true, src: 'https://i.pravatar.cc/300?img=1', alt: 'Avatar 1' },
+            { fallback: true, src: 'https://i.pravatar.cc/300?img=2', alt: 'Avatar 2' },
+            { fallback: false, src: 'https://i.pravatar.cc/300?img=3', alt: 'Avatar 3' },
+        ];
+        render(<AvatarGroup avatars={brokenFallBack} />);
+        avatars.forEach(avatar => {
+            const img = screen.getByAltText(avatar.alt);
+            expect(img).toHaveAttribute('src', avatar.src);
+        });
+    });
+
+    test('AvatarGroup renders for broken image src', () => {
+        render(<AvatarGroup avatars={avatars} />);
+        avatars.forEach(avatar => {
+            const img = screen.getByAltText(avatar.alt);
+            fireEvent.error(img);
+            // Assert that the fallback text is rendered 
+            expect(screen.getByText('A')).toBeInTheDocument();  
+        });
+    }); 
 });
 
