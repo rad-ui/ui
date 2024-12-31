b09e891624f7298053126ff0bbf6b92673231ec4
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com.>
2024-12-31 12:07:17 +0530

    AvatarGroup Docs

--- docs/app/docs/components/avatar-group/docs/codeUsage.js
+++ docs/app/docs/components/avatar-group/docs/codeUsage.js
@@ -1,13 +1,13 @@
 import Kbd from '@radui/ui/Kbd';
 import Text from '@radui/ui/Text';
 import { getSourceCodeFromPath } from '@/utils/parseSourceCode';
-const COMPONEN_FOLDER = 'avatar-group';
-const example_1_SourceCode = await getSourceCodeFromPath(`docs/app/docs/components/${COMPONEN_FOLDER}/docs/example_1.tsx`);
+const COMPONENT_FOLDER = 'avatar-group';
+const example_1_SourceCode = await getSourceCodeFromPath(`docs/app/docs/components/${COMPONENT_FOLDER}/docs/example_1.tsx`);
 // console.log(example_1_SourceCode);
 
 
-const scss_SourceCode = await getSourceCodeFromPath(`styles/themes/components/${COMPONEN_FOLDER}.scss`);
-const anatomy_SourceCode = await getSourceCodeFromPath(`docs/app/docs/components/${COMPONEN_FOLDER}/docs/accordion_anatomy.tsx`);
+const scss_SourceCode = await getSourceCodeFromPath(`styles/themes/components/${COMPONENT_FOLDER}.scss`);
+const anatomy_SourceCode = await getSourceCodeFromPath(`docs/app/docs/components/${COMPONENT_FOLDER}/docs/accordion_anatomy.tsx`);
 
 export const code = {
     javascript: {
