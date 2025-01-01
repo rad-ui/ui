14de37ec83d4308323003a3148cbbe9eb3842f15
Huy Vu <vuquanghuy2k@gmail.com>
2025-01-01 08:27:11 +0700

    Fix #699: Update test for Code component align with previous tests

--- src/components/ui/Code/tests/Code.test.tsx
+++ src/components/ui/Code/tests/Code.test.tsx
@@ -4,10 +4,10 @@ import Code from '../Code';
 
 describe('Code Component', () => {
     it('renders without crashing', () => {
-        render(<Code>console.log('Hello world!');</Code>);
+        const {container} = render(<Code>console.log('Hello world!');</Code>);
 
-        const container = document.querySelector('code');
-        expect(container).toBeTruthy();
-        getByText(container!, "console.log('Hello world!');")
+        const codeElement = container.querySelector('code');
+        expect(codeElement).toBeInTheDocument();
+        expect(codeElement).toHaveTextContent("console.log('Hello world!');");
     });
 })
