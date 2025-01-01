d849ef83519c3ce3f5a6e4c8bc81e081f513155b
Huy Vu <vuquanghuy2k@gmail.com>
2025-01-01 09:14:31 +0700

    Fix #699: Remove redundant import

--- src/components/ui/Code/tests/Code.test.tsx
+++ src/components/ui/Code/tests/Code.test.tsx
@@ -1,10 +1,10 @@
-import { render, getByText } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import React from 'react';
 import Code from '../Code';
 
 describe('Code Component', () => {
     it('renders without crashing', () => {
-        const {container} = render(<Code>console.log('Hello world!');</Code>);
+        const { container } = render(<Code>console.log('Hello world!');</Code>);
 
         const codeElement = container.querySelector('code');
         expect(codeElement).toBeInTheDocument();
