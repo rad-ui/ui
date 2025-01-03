d30a5bf0ad1791bd7d210ce5c6812e8a1998164a
Huy Vu <vuquanghuy2k@gmail.com>
2025-01-03 09:02:36 +0700

    Fix #735: Update code format

--- src/components/ui/Table/tests/Table.test.tsx
+++ src/components/ui/Table/tests/Table.test.tsx
@@ -5,17 +5,17 @@ import Table from '../Table';
 
 describe('Table Component', () => {
     const employeeData = [
-        {id: 1, fullName: 'John Smith', age: 23, isIntern: false},
-        {id: 2, fullName: 'Anna Donie', age: 35, isIntern: true},
-        {id: 3, fullName: 'Hannah Brown', age: 20, isIntern: true},
-        {id: 4, fullName: 'Johnathan White Jr', age: 36, isIntern: false},
+        { id: 1, fullName: 'John Smith', age: 23, isIntern: false },
+        { id: 2, fullName: 'Anna Donie', age: 35, isIntern: true },
+        { id: 3, fullName: 'Hannah Brown', age: 20, isIntern: true },
+        { id: 4, fullName: 'Johnathan White Jr', age: 36, isIntern: false },
     ];
 
     const employeeKey = [
-        {key: 1, name: 'Employee Id'},
-        {key: 2, name: 'Full Name'},
-        {key: 3, name: 'Age'},
-        {key: 4, name: 'In Internship'},
+        { key: 1, name: 'Employee Id' },
+        { key: 2, name: 'Full Name' },
+        { key: 3, name: 'Age' },
+        { key: 4, name: 'In Internship' },
     ];
 
     let result: RenderResult;
@@ -33,16 +33,16 @@ describe('Table Component', () => {
         expect(tableElements.length).toEqual(1);
         expect(tableElements[0]).toBeTruthy();
     });
-    
+
     it('renders correct user defined headers', () => {
         const headerRows = container.querySelectorAll('table thead tr');
         expect(headerRows.length).toEqual(1);
-        
+
         const headers = headerRows[0].querySelectorAll('th');
         expect(headers.length).toEqual(4);
 
         headers.forEach((header, index) => {
-            expect(header.textContent)  .toEqual(employeeKey[index].name);
+            expect(header.textContent).toEqual(employeeKey[index].name);
         });
     });
 
@@ -58,7 +58,9 @@ describe('Table Component', () => {
             expect(dataCells.length).toEqual(4);
 
             dataCells.forEach((cell, cellIndex) => {
-                cell.textContent = Object.values(employeeData[rowIndex])[cellIndex].toString();
+                cell.textContent = Object.values(employeeData[rowIndex])[
+                    cellIndex
+                ].toString();
             });
         });
     });
