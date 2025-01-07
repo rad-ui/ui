0642fba729b05acf0b3818ce9c49dd226eedadd0
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com>
2025-01-07 10:50:22 +0530

    tests and refactoring for toggle and toggleGroup component (#755)

--- src/components/ui/ToggleGroup/contexts/toggleContext.tsx
+++ src/components/ui/ToggleGroup/contexts/toggleContext.tsx
@@ -4,6 +4,14 @@ export type ToggleContextType = {
     type: 'single' | 'multiple';
     activeToggles: any[];
     setActiveToggles: (toggles: any[]) => void;
+    nextItem: () => void;
+    previousItem: () => void;
 };
 
-export const ToggleContext = createContext<ToggleContextType>({});
+export const ToggleContext = createContext<ToggleContextType>({
+    type: 'single',
+    activeToggles: [],
+    setActiveToggles: () => {},
+    nextItem: () => {},
+    previousItem: () => {},
+});
