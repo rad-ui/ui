d970cdd6d19ced4b95547c0bf6469ac80a8a295b
StDensity <noyaljosephnnj@gmail.com>
2025-01-09 11:03:46 +0530

    refactor: Simplify CodeWithCopy component to support inline only

--- docs/components/CodeWithCopy.js
+++ docs/components/CodeWithCopy.js
@@ -3,27 +3,14 @@ import Copy from "@/components/Copy";
 import Tooltip from "@radui/ui/Tooltip";
 
 const CodeWithCopy = ({ code }) => {
-  const copyContent = code
-    .replace(/\n{2,}/g, "\n") // Replace multiple newlines with single newline
-    .trim(); // Remove leading/trailing whitespace
-
   return (
     <Code>
-      <span className="flex items-center relative min-h-[2.06rem]">
-        <span
-          className={`whitespace-pre-wrap mr-10`}
-          style={{ wordBreak: "break-word" }}
-        >
-          {code}
+       <span  className="flex items-center">
+            <span className="mr-2">
+                {code}
+            </span>
+                <Copy content={code} />
         </span>
-        <Tooltip
-          label="Copy"
-          placement="bottom"
-          className="absolute top-0 right-0"
-        >
-          <Copy content={copyContent} />
-        </Tooltip>
-      </span>
     </Code>
   );
 };
