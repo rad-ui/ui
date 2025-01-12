3239227ab7542411b86168e320a8676c76cfcb65
StDensity <noyaljosephnnj@gmail.com>
2025-01-12 17:40:09 +0530

    Increase stroke width for HamburgerIcon to match the XIcon and clean up XIcon SVG structure

--- docs/components/navigation/Navigation.js
+++ docs/components/navigation/Navigation.js
@@ -206,7 +206,7 @@ const HamburgerIcon = (props) => (
           d="M13.5 2H.5M13.5 7H.5M13.5 12H.5"
           style={{
             stroke: "#ab4aba",
-            strokeWidth: 1,
+            strokeWidth: 2,
             strokeDasharray: "none",
             strokeLinecap: "round",
             strokeDashoffset: 0,
@@ -224,17 +224,15 @@ const HamburgerIcon = (props) => (
 
   const XIcon = (props) => (
     <svg
-     className='text-red-200'
-    xmlns="http://www.w3.org/2000/svg"
-    // xmlSpace="preserve"
-    width={18}
-    height={18}
-    
-   fill='#ab4aba'
-   stroke='#ab4aba'
-    viewBox="0 0 460.775 460.775"
-    {...props}
-  >
+        xmlns="http://www.w3.org/2000/svg"
+        // xmlSpace="preserve"
+        width={18}
+        height={18}
+        fill='#ab4aba'
+        stroke='#ab4aba'
+        viewBox="0 0 460.775 460.775"
+        {...props}
+    >
     <path d="M285.08 230.397 456.218 59.27c6.076-6.077 6.076-15.911 0-21.986L423.511 4.565a15.55 15.55 0 0 0-21.985 0l-171.138 171.14L59.25 4.565a15.551 15.551 0 0 0-21.985 0L4.558 37.284c-6.077 6.075-6.077 15.909 0 21.986l171.138 171.128L4.575 401.505c-6.074 6.077-6.074 15.911 0 21.986l32.709 32.719a15.555 15.555 0 0 0 21.986 0l171.117-171.12 171.118 171.12a15.551 15.551 0 0 0 21.985 0l32.709-32.719c6.074-6.075 6.074-15.909 0-21.986L285.08 230.397z" />
   </svg>
   )
