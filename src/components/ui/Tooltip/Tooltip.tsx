849205d1b1dede83e394ac4b2edaca6f9fd404eb
foobar <uagnl25x@anonaddy.me>
2025-01-14 22:52:41 +0530

    Fix (#781): props have more strict type now

--- src/components/ui/Tooltip/Tooltip.tsx
+++ src/components/ui/Tooltip/Tooltip.tsx
@@ -1,15 +1,13 @@
-import { Placement } from '@floating-ui/react';
 import React from 'react';
-import Popper from '~/components/tools/Popper/Popper';
+import Popper, { PopperProps } from '~/components/tools/Popper/Popper';
 
 const COMPONENT_NAME = 'Tooltip';
 
 type TooltipProps = {
-    children: React.ReactNode;
-    label?: string;
-    placement?:Placement;
-    [key: string]: any;
-};
+  children: React.ReactNode;
+  label?: string;
+  placement?: PopperProps['placement'];
+} & JSX.IntrinsicElements['span'];
 
 const Tooltip = ({ children, label = '', placement = 'top', ...props }: TooltipProps) => {
     return (
