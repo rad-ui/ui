e6a30a2a597a38740d36331f427feee682b51a31
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com.>
2025-01-17 20:35:21 +0530

    Button color prop docs

--- docs/app/docs/components/button/examples/ButtonColor.tsx
+++ docs/app/docs/components/button/examples/ButtonColor.tsx
@@ -4,7 +4,6 @@ import Button from '@radui/ui/Button';
 const ButtonColor = () => {
     const sizes = ['small', 'medium', 'large', 'x-large'];
     const variants = ['solid', 'soft', 'outline', 'ghost'];
-    // const colors = ['yellow', 'black', 'pink', 'brown']
     return <div className='flex flex-col gap-4'>
         {variants.map((variant, index) => {
             return <div key={index} className='flex items-center gap-4'>
