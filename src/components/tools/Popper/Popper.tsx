624384dffa1351cb871f4246dae1c5ba5163217b
foobar <uagnl25x@anonaddy.me>
2025-01-19 22:56:29 +0530

    chore: remove unused imports and variables

--- src/components/tools/Popper/Popper.tsx
+++ src/components/tools/Popper/Popper.tsx
@@ -1,8 +1,4 @@
-import React, { useState, useRef } from 'react';
-import { customClassSwitcher } from '~/core';
-import { clsx } from 'clsx';
-
-import { useFloating, useInteractions, useHover, FloatingArrow, arrow, offset, flip, autoPlacement, hide, shift, autoUpdate, useRole, useDismiss, FloatingPortal, Placement } from '@floating-ui/react';
+import React from 'react';
 
 import PopperRoot from './fragments/PopperRoot';
 import PopperContent from './fragments/PopperContent';
@@ -12,11 +8,7 @@ import type { PopperRootProps } from './fragments/PopperRoot';
 import type { PopperContentProps } from './fragments/PopperContent';
 import type { PopperTriggerProps } from './fragments/PopperTrigger';
 
-// TODO : Use Floating Portal?
 // TODO : Collisions dont seem to be working as expected, need to investigate
-
-const ARROW_HEIGHT = 7;
-const GAP = 2;
 
 /**
  *
