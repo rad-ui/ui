4d97a7667c0699af47129dddbbadc01d39204824
foobar <uagnl25x@anonaddy.me>
2025-01-20 21:56:01 +0530

    fix: improve style for story

--- src/components/tools/Popper/stories/Popper.stories.tsx
+++ src/components/tools/Popper/stories/Popper.stories.tsx
@@ -14,15 +14,26 @@ const meta: Meta<typeof Popper> = {
 
 export default meta;
 
-export const Defult: Story = {
-    render: ({}) => (
-        <SandboxEditor className=''>
-            This is an example for popper
-            <Popper pop={'hello world'}>
-                <div>
-                      Hover Over Me
-                </div>
-            </Popper>
-        </SandboxEditor>
-    )
+export const Default: Story = {
+    render: ({ }) => {
+        return (
+            <SandboxEditor>
+                <Popper.Root
+                    placement={'right'}
+                >
+                    <Popper.Trigger
+                        className="inline-block rounded bg-indigo-600 px-8 py-3 text-sm font-medium text-white transition hover:scale-110 hover:shadow-xl"
+                    > See Quote </Popper.Trigger>
+
+                    <Popper.Content showArrow={true} className='bg-indigo-600 rounded max-w-[30ch] p-4'>
+                        <blockquote cite="https://www.imdb.com/title/tt0373732/characters/nm0936762/">
+                            <p> You can’t change the world, but you gotta at least try to change your part in it. - Robert Freeman, Boondocks </p>
+                        </blockquote>
+                        <p className='mt-3'>{'—Robert \'Granddad\' Freeman, '}<cite>The Boondocks</cite></p>
+
+                    </Popper.Content>
+                </Popper.Root>
+            </SandboxEditor>
+        );
+    }
 };
