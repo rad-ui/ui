0bb1e4d942131263d8fae2cb64ea2f3bb68aed3e
foobar <uagnl25x@anonaddy.me>
2025-01-21 09:44:07 +0530

    feat: Add new story and use button primitive

--- src/components/ui/Tooltip/stories/Tooltip.stories.tsx
+++ src/components/ui/Tooltip/stories/Tooltip.stories.tsx
@@ -4,6 +4,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import Tooltip from '../Tooltip';
 import SandboxEditor from '~/components/tools/SandboxEditor/SandboxEditor';
 import { Placement } from '@floating-ui/react';
+import Primitive from '~/core/primitives/Primitive';
 
 const placement = ['top', 'right', 'bottom', 'left', 'top-start', 'top-end', 'right-start', 'right-end', 'bottom-start', 'bottom-end', 'left-start', 'left-end'] satisfies Placement[];
 
@@ -16,13 +17,27 @@ export default meta;
 
 type Story = StoryObj<typeof Tooltip>;
 
-export const Basic: Story = {
+export const WithArrow: Story = {
     render: () => (
         <SandboxEditor>
             <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] justify-center gap-3">
                 {placement.map((p) => (
-                    <Tooltip label={p} placement={p} key={p} className='capitalize border border-neutral-600 p-4 rounded-md'>
-                        <span>{p}</span>
+                    <Tooltip label={p} className="rounded border border-neutral-400 capitalize p-2" placement={p} key={p}>
+                        <Primitive.button >{p}</Primitive.button>
+                    </Tooltip>
+                ))}
+            </div>
+        </SandboxEditor>
+    )
+};
+
+export const WithoutArrow: Story = {
+    render: () => (
+        <SandboxEditor>
+            <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] justify-center gap-3">
+                {placement.map((p) => (
+                    <Tooltip label={p} className="rounded border border-neutral-400 capitalize p-2" placement={p} key={p} showArrow={false}>
+                        <Primitive.button>{p}</Primitive.button>
                     </Tooltip>
                 ))}
             </div>
