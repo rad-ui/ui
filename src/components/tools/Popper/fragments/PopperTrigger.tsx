8b556799b57d2687f1684a99676ad76f68cf5782
foobar <uagnl25x@anonaddy.me>
2025-01-21 14:35:29 +0530

    fix (api): add asChild as a prop to popper.trigger

--- src/components/tools/Popper/fragments/PopperTrigger.tsx
+++ src/components/tools/Popper/fragments/PopperTrigger.tsx
@@ -5,15 +5,16 @@ import Primitive from '~/core/primitives/Primitive';
 
 export type PopperTriggerProps = PropsWithChildren<{
   className?: string;
+  asChild?: boolean
 }>;
 
-export default function PopperTrigger({ children, className = '', ...props }: PopperTriggerProps) {
+export default function PopperTrigger({ asChild, children, className = '', ...props }: PopperTriggerProps) {
     const { rootClass, floating, interactions } = usePopper();
     const { refs: { setReference } } = floating;
     const { getReferenceProps } = interactions;
 
     return (
-        <Primitive.span asChild={isValidElement(children)} className={clsx('rad-ui-popper', `${rootClass}-reference-element`, className)} ref={setReference} {...getReferenceProps()}>
+        <Primitive.span asChild={asChild} className={clsx('rad-ui-popper', `${rootClass}-reference-element`, className)} ref={setReference} {...getReferenceProps()}>
             {children}
         </Primitive.span>)
     ;
