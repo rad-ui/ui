c8ec46688f9123f113775790a31ca88b127edef7
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com>
2025-01-21 10:20:22 +0530

    BlockQuote color prop with test, docs and storybook (#797)
    Co-authored-by: Pranay Kothapalli <pranaykothapalli@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- src/components/ui/BlockQuote/BlockQuote.tsx
+++ src/components/ui/BlockQuote/BlockQuote.tsx
@@ -9,12 +9,19 @@ export type BlockQuoteProps = {
     children: React.ReactNode;
     customRootClass?: string;
     className?: string;
+    color?:string;
     props?: Record<string, any>[]
 }
-const BlockQuote = ({ children, customRootClass = '', className = '', ...props }: BlockQuoteProps) => {
+const BlockQuote = ({ children, customRootClass = '', className = '', color = '', ...props }: BlockQuoteProps) => {
     const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME);
 
-    return <blockquote className={clsx(rootClass, className)} {...props}>{children}</blockquote>;
+    const data_attributes: Record<string, string> = {};
+
+    if (color) {
+        data_attributes['data-accent-color'] = color;
+    }
+
+    return <blockquote className={clsx(rootClass, className)} {...props} {...data_attributes}> {children}</blockquote>;
 };
 
 BlockQuote.displayName = COMPONENT_NAME;
