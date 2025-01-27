c9f932dc4ba8f60275b77aa80fa53a54e4086d2a
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com.>
2025-01-27 22:10:13 +0530

    migration b2

--- src/components/ui/Disclosure/stories/Disclosure.stories.tsx
+++ src/components/ui/Disclosure/stories/Disclosure.stories.tsx
@@ -1,10 +1,12 @@
-import Disclosure from '../Disclosure'
+import { JSX } from 'react';
+import Disclosure, { DisclosureProps } from '../Disclosure'
 import SandboxEditor from '~/components/tools/SandboxEditor/SandboxEditor';
+import React from 'react';
 
 export default {
     title: 'Components/Disclosure',
     component: Disclosure,
-    render: (args) => <SandboxEditor>
+    render: (args: JSX.IntrinsicAttributes & DisclosureProps) => <SandboxEditor>
          
             <div>
                 <Disclosure {...args} />
