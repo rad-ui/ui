5bb3def521fbecee036ff127ee17b302b3811173
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-02-08 20:17:16 +0530

    Add Radix UI reference for useLayoutEffect implementation

--- src/core/hooks/useLayoutEffect/index.tsx
+++ src/core/hooks/useLayoutEffect/index.tsx
@@ -4,6 +4,9 @@ import { useLayoutEffect as ReactUseLayoutEffect } from 'react';
 // One of the hooks that do not work on server components
 // wrapping it this way and using it will make sure errors are not thrown and fail silently
 // If it's server, we just return a noop function (no operation)
+
+//  Radix UI does it this way - https://github.com/radix-ui/primitives/blob/main/packages/react/use-layout-effect/src/use-layout-effect.tsx
+//
 const useLayoutEffect = globalThis.document ? ReactUseLayoutEffect : () => {};
 
 export default useLayoutEffect;
