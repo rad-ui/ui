89543df134805f7ac60edbcb280f3ee01c726887
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com.>
2025-02-18 23:46:53 +0530

    starting

---
+++ docs/app/testing/layout/page.tsx
@@ -0,0 +1,14 @@
+
+
+const layout = () => {
+    return (
+        <div className="m-auto md:max-w-[1440px]">
+            <div className="bg-red-400">
+                    Component Name
+            </div>
+            layout
+        </div>
+    )
+}
+
+export default layout;
