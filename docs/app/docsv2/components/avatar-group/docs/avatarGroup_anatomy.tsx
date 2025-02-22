e894593017ed3b83279e1103e977d1ae11e00db2
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-02-22 19:23:31 +0530

    Add Avatar Group and Anatomy components for documentation

---
+++ docs/app/docsv2/components/avatar-group/docs/avatarGroup_anatomy.tsx
@@ -0,0 +1,12 @@
+import AvatarGroup from "@radui/ui/AvatarGroup";
+
+export default ()=>{
+    return (
+        <AvatarGroup.Root> 
+            <AvatarGroup.AvatarRoot>
+                <AvatarGroup.AvatarImage />
+                <AvatarGroup.AvatarFallback />
+            </AvatarGroup.AvatarRoot>
+        </AvatarGroup.Root>
+    )
+}
