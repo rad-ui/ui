10ddf3f0d2e5cbf19c3f941d8b1351bd8f5019e0
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-02-23 19:43:45 +0530

    Add code usage examples for Text, Heading, and Kbd components

---
+++ docs/app/docsv2/components/kbd/docs/codeUsage.js
@@ -0,0 +1,29 @@
+const code = {
+    javascript: {
+        code: `import Kbd from "@radui/ui/Kbd";
+
+const KbdExample = () => (
+  <Kbd>Ctrl + C</Kbd>
+)
+`
+    },
+    css: {
+        code: `.rad-ui-kbd{
+    user-select: none;
+    font-weight: 400;
+    font-family: inherit;
+    background-color: var(--rad-ui-color-gray-50);
+    color: var(--rad-ui-color-gray-950);
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    white-space: nowrap;
+    border-radius: 4px;
+    border: 1px solid var(--rad-ui-color-gray-500);
+    box-shadow:  0px 0px 3px 1px var(--rad-ui-color-gray-400);
+    padding: 4px 8px;
+}`
+    }
+};
+
+export default code;
