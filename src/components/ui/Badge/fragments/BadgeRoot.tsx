7119dd5a05781d828daae8c119753dbbdcea03fc
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com.>
2025-03-03 23:16:37 +0530

    Badge variants and sizes

--- src/components/ui/Badge/fragments/BadgeRoot.tsx
+++ src/components/ui/Badge/fragments/BadgeRoot.tsx
@@ -7,15 +7,30 @@ type BadgeRootProps = {
      children: React.ReactNode;
      customRootClass?: string;
      color?: string;
+     variant?: string;
+     size?:string;
      className?: string;
      props?: Record<string, any>[]
 }
 
-const BadgeRoot = ({ children, customRootClass, className, color, ...props }:BadgeRootProps) => {
+const BadgeRoot = ({ children, customRootClass, className, variant = '', size = '', color, ...props }:BadgeRootProps) => {
     const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME);
+    const data_attributes: Record<string, string> = {};
+
+    if (variant) {
+        data_attributes['data-button-variant'] = variant;
+    }
+
+    if (size) {
+        data_attributes['data-button-size'] = size;
+    }
+
+    if (color) {
+        data_attributes['data-accent-color'] = color;
+    }
 
     return (
-        <span className={clsx(rootClass, className)} data-accent-color={color ?? undefined} {...props}>
+        <span className={clsx(rootClass, className)} {...data_attributes} {...props}>
             {children}
         </span>
     );
--- src/components/ui/Badge/stories/Badge.stories.tsx
+++ src/components/ui/Badge/stories/Badge.stories.tsx
@@ -6,6 +6,9 @@ import React from 'react';
 const ArrowIcon = ({ className }: { className:string}) => {
     return <svg className={className} width="16" height="16" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>;
 };
+const BadgeText = 'Badge';
+const Variants = ['solid', 'soft', 'outline', 'ghost'];
+const Sizes = ['small', 'medium', 'large', 'x-large'];
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -13,13 +16,13 @@ export default {
     component: Badge,
     render: () => <SandboxEditor>
         <div className='flex space-x-2'>
-            <Badge className='text-xs' >Plum</Badge>
+            <Badge  >Plum</Badge>
             <Separator orientation="vertical" />
-            <Badge className='text-xs' color="plum" >Plum</Badge>
-            <Badge className='text-xs' color="red" >Red</Badge>
-            <Badge className='text-xs' color="gray" >Gray</Badge>
+            <Badge  color="plum">Plum</Badge>
+            <Badge  color="red" >Red</Badge>
+            <Badge  color="gray" >Gray</Badge>
             <Separator orientation="vertical" />
-            <Badge className='text-xs space-x-1' >
+            <Badge  >
 
                 <div>
                     With Icon
@@ -35,4 +38,44 @@ export const All = {
     args: {
         className: ''
     }
+};
+
+export const Size = () => {
+    return <SandboxEditor>
+        <div className='mt-4 mb-2'>
+            <p className='text-gray-950'>Badge Size</p>
+        </div>
+        <div className=''>
+
+            {Variants.map((variant, index) => (
+                <div key={index} className='mb-10'>
+                    <span key={index} className="inline-flex items-start space-x-2">
+                        {Sizes.map((size, index) => {
+                            return <Badge key={index} size={size} variant={variant} >
+                                <div>{BadgeText} </div>
+                            </Badge>;
+                        })}
+                    </span>
+                </div>
+            ))}
+
+        </div>
+    </SandboxEditor>;
+};
+
+export const Variant = () => {
+    return <SandboxEditor>
+        <div className='mt-4 mb-2'>
+            <p className='text-gray-950'>Badge Variant</p>
+        </div>
+        <div className='flex space-x-2'>
+
+                        {Variants.map((variant, index) => {
+                            return <Badge key={index}  variant={variant} >
+                                <div>{BadgeText} </div>
+                            </Badge>;
+                        })}
+
+        </div>
+    </SandboxEditor>;
 };
