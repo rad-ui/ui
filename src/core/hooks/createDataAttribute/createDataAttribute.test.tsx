803772ddf8546ff8e21ed620005de84eba002699
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com.>
2025-03-19 00:58:03 +0530

    updated tests

--- src/core/hooks/createDataAttribute/createDataAttribute.test.tsx
+++ src/core/hooks/createDataAttribute/createDataAttribute.test.tsx
@@ -1,28 +1,43 @@
+import { renderHook } from "@testing-library/react";
 import { useCreateDataAttribute, useComposeAttributes } from ".";
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import Button from "~/components/ui/Button/Button";
 
-describe("createDataAttribute", () => {
-    
-  test("attributes are created and applied", () => {
-    const variant = "primary";
-    const size = "large";
-    const color = "red";
-    
-    const dataAttributes = useCreateDataAttribute("button", { variant, size });
-    const accentAttributes = useCreateDataAttribute("accent", { color });
-    const composedAttributes = useComposeAttributes(
-      dataAttributes,
-      accentAttributes
-    );
-    const Component = () => <Button {...composedAttributes}>Click me</Button>;
-    render(<Component />);
+test("attributes are created and applied", () => {
+  const { result: dataAttributes } = renderHook(() =>
+    useCreateDataAttribute("button", { variant: "primary", size: "large" })
+  );
 
-    expect(screen.getByText("Click me")).toBeInTheDocument();
-    expect(screen.getByText("Click me")).toHaveAttribute("data-button-variant","primary");
-    expect(screen.getByText("Click me")).toHaveAttribute("data-button-size","large");
-    expect(screen.getByText("Click me")).toHaveAttribute("data-accent-color","red");
+  const { result: accentAttributes } = renderHook(() =>
+    useCreateDataAttribute("accent", { color: "red" })
+  );
+
+  const { result: composedAttributes } = renderHook(() =>
+    useComposeAttributes(dataAttributes.current, accentAttributes.current)
+  );
+
+  expect(composedAttributes.current).toEqual({
+    "data-button-variant": "primary",
+    "data-button-size": "large",
+    "data-accent-color": "red",
   });
-
 });
+
+  test("attributes are created and applied", () => {
+    const { result: dataAttributes } = renderHook(() =>
+      useCreateDataAttribute("button", { variant: undefined, size: "large" })
+    );
+  
+    const { result: accentAttributes } = renderHook(() =>
+      useCreateDataAttribute("accent", { color: "" })
+    );
+  
+    const { result: composedAttributes } = renderHook(() =>
+      useComposeAttributes(dataAttributes.current, accentAttributes.current)
+    );
+  
+    expect(composedAttributes.current).toEqual({
+      "data-button-size": "large",
+      
+    });
+  })
+  
+
