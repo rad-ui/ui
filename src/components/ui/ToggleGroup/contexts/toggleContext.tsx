9e2894c2387c4f3ac582872b3e779cb0d608ba78
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-03-20 06:21:03 +0530

    Remove unnecessary functions in ToggleContext

--- src/components/ui/ToggleGroup/contexts/toggleContext.tsx
+++ src/components/ui/ToggleGroup/contexts/toggleContext.tsx
@@ -4,14 +4,10 @@ export type ToggleContextType = {
     type: 'single' | 'multiple';
     activeToggles: any[];
     setActiveToggles: (toggles: any[]) => void;
-    nextItem: () => void;
-    previousItem: () => void;
 };
 
 export const ToggleContext = createContext<ToggleContextType>({
     type: 'single',
     activeToggles: [],
-    setActiveToggles: () => {},
-    nextItem: () => {},
-    previousItem: () => {}
+    setActiveToggles: () => {}
 });
