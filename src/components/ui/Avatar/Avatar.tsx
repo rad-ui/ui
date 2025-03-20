3f5bddf07b7da04805a3b05acb139052a9ec397b
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com.>
2025-03-20 23:31:31 +0530

    Avatar data api support

--- src/components/ui/Avatar/Avatar.tsx
+++ src/components/ui/Avatar/Avatar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { clsx } from 'clsx';
 import AvatarPrimitive from '~/core/primitives/Avatar';
+import { useCreateDataAttribute, useComposeAttributes } from '~/core/hooks/createDataAttribute';
 
 const COMPONENT_NAME = 'Avatar';
 
@@ -9,15 +10,20 @@ export type AvatarProps = {
     customRootClass?: string,
     fallback?: string,
     className?: string,
+    variant?: string;
+    size?:string;
     src?: string,
     alt?: string,
     color?:string,
     props?: Record<string, any>[]
 }
 
-const Avatar = ({ customRootClass = '', fallback, className, src, alt, color, ...props }: AvatarProps) => {
+const Avatar = ({ customRootClass = '', fallback, className, src, alt, variant = '', size = '', color, ...props }: AvatarProps) => {
+    const dataAttributes = useCreateDataAttribute('avatar', { variant, size });
+    const accentAttributes = useCreateDataAttribute('accent', { color });
+    const composedAttributes = useComposeAttributes(dataAttributes(), accentAttributes());
     return (
-        <AvatarPrimitive.Root src={src} customRootClass={customRootClass}>
+        <AvatarPrimitive.Root src={src} customRootClass={customRootClass}  {...composedAttributes()}>
             <AvatarPrimitive.Image
                 src={src}
                 alt={alt}
