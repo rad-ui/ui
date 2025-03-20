dd8188f9ef27b88fa5fc07e567899764daab23c3
jyoti <jindaljyoti913@gmail.com>
2025-03-20 10:36:16 -0700

    add variant and size for kbd

--- src/components/ui/Kbd/Kbd.tsx
+++ src/components/ui/Kbd/Kbd.tsx
@@ -2,18 +2,26 @@
 import React from 'react';
 import { customClassSwitcher } from '~/core';
 import { clsx } from 'clsx';
+import { useCreateDataAttribute } from '~/core/hooks/createDataAttribute';
 const COMPONENT_NAME = 'Kbd';
 
 export type KbdProps = {
     children: React.ReactNode;
     customRootClass?: string;
     className?: string;
+    size?: string;
     props: Record<string, any>[];
 }
 
-const Kbd = ({ children, customRootClass, className, ...props }: KbdProps) => {
+const Kbd = ({ children, customRootClass, className, size= '', ...props }: KbdProps) => {
     const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME);
-    return <kbd className={clsx(rootClass, className)} {...props}>{children}</kbd>;
+    
+    const data_attributes: Record<string, string> = {};
+
+    if (size) {
+        data_attributes['data-kbd-size'] = size;
+    }
+    return <kbd className={clsx(rootClass, className)} {...data_attributes} {...props}>{children}</kbd>;
 };
 
 export default Kbd;
--- src/components/ui/Kbd/stories/Kbd.stories.tsx
+++ src/components/ui/Kbd/stories/Kbd.stories.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Kbd, { KbdProps } from '../Kbd';
 import SandboxEditor from '~/components/tools/SandboxEditor/SandboxEditor';
 
+const Sizes = ['small', 'medium', 'large', 'x-large']
+
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
     title: 'Components/Kbd',
@@ -18,4 +20,21 @@ export const All = {
     args: {
         className: 'text-xs'
     }
+};
+
+export const Size = () => {
+    return <SandboxEditor>
+        <div className='mt-4 mb-2'>
+            <p className='text-gray-950'>Kbd Size</p>
+        </div>
+        <div>
+
+               <div className="inline-flex items-start space-x-2">
+                        {Sizes.map((size, index) => {
+                            return <Kbd key={index} size={size}>Ctrl + X</Kbd>;
+                        })}
+               </div>
+
+        </div>
+    </SandboxEditor>;
 };
