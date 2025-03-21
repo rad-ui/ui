b80534cb0087cb7c45c3c98e348e9a2177e076ae
jindaljyoti <137828895+jindaljyoti@users.noreply.github.com>
2025-03-21 07:21:57 -0700

    add size for kbd component (#942)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- src/components/ui/Kbd/Kbd.tsx
+++ src/components/ui/Kbd/Kbd.tsx
@@ -2,18 +2,23 @@
 import React from 'react';
 import { customClassSwitcher } from '~/core';
 import { clsx } from 'clsx';
+import { useCreateDataAttribute } from '~/core/hooks/createDataAttribute';
 const COMPONENT_NAME = 'Kbd';
 
 export type KbdProps = {
     children: React.ReactNode;
     customRootClass?: string;
     className?: string;
+    size?: string;
     props: Record<string, any>[];
 }
 
-const Kbd = ({ children, customRootClass, className, ...props }: KbdProps) => {
+const Kbd = ({ children, customRootClass, className, size= '', ...props }: KbdProps) => {
     const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME);
-    return <kbd className={clsx(rootClass, className)} {...props}>{children}</kbd>;
+    
+    const dataAttributes = useCreateDataAttribute('kbd', { size })
+
+    return <kbd className={clsx(rootClass, className)} {...dataAttributes()} {...props}>{children}</kbd>;
 };
 
 export default Kbd;
