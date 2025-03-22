ada321cc8d31252534bd05b0305ecf526bc89a0e
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-03-22 19:07:58 +0530

    Add display name to Table components (#956)

--- src/components/ui/Table/fragments/TableBody.tsx
+++ src/components/ui/Table/fragments/TableBody.tsx
@@ -2,10 +2,14 @@
 import React from 'react';
 import { clsx } from 'clsx';
 
+const COMPONENT_NAME = 'TableBody';
+
 const TableBody = ({ children, className = '', ...props }:any) => {
     return <tbody className={clsx(className)} {...props} >
         {children}
     </tbody>;
 };
+
+TableBody.displayName = COMPONENT_NAME;
 
 export default TableBody;
