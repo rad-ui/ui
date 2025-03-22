8590f66caa6aeaf2602bb8005fe1be96f3d68367
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-03-22 08:50:38 +0530

    Remove unused focus related functions from AccordionContext

--- src/components/ui/Accordion/contexts/AccordionContext.tsx
+++ src/components/ui/Accordion/contexts/AccordionContext.tsx
@@ -3,23 +3,15 @@ import { createContext } from 'react';
 interface AccordionContextType {
     rootClass?: string | null;
     activeItem?: number | null;
-    focusItem?: Element | null;
     setActiveItem: (item: number | null) => void;
-    setFocusItem: (item: Element) => void;
-    focusNextItem: () => void;
-    focusPrevItem: () => void;
     accordionRef?: React.RefObject<HTMLDivElement | null>;
     transitionDuration?: number;
-  }
+}
 
 export const AccordionContext = createContext<AccordionContextType>({
     rootClass: '',
     activeItem: null,
-    focusItem: null,
     setActiveItem: () => {},
-    setFocusItem: () => {},
-    focusNextItem: () => {},
-    focusPrevItem: () => {},
     accordionRef: undefined,
     transitionDuration: 0
 });
