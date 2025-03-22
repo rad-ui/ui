97359b34b893f2119dfdd0d4517aee60fcba4ac8
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-03-22 19:07:04 +0530

    Add display name to Table components

--- src/components/ui/Table/fragments/TableCell.tsx
+++ src/components/ui/Table/fragments/TableCell.tsx
@@ -2,10 +2,14 @@
 import React from 'react';
 import { clsx } from 'clsx';
 
+const COMPONENT_NAME = 'TableCell';
+
 const TableCell = ({ children, className = 'cell', ...props }:any) => {
     return <td className={clsx(className)} {...props} >
         {children}
     </td>;
 };
+
+TableCell.displayName = COMPONENT_NAME;
 
 export default TableCell;
--- src/components/ui/Table/fragments/TableColumnCellHeader.tsx
+++ src/components/ui/Table/fragments/TableColumnCellHeader.tsx
@@ -2,10 +2,14 @@
 import React from 'react';
 import { clsx } from 'clsx';
 
+const COMPONENT_NAME = 'TableColumnCellHeader';
+
 const TableColumnCellHeader = ({ children, className = 'cell-header', ...props }:any) => {
     return <th className={clsx(className)} {...props}>
         {children}
     </th>;
 };
+
+TableColumnCellHeader.displayName = COMPONENT_NAME;
 
 export default TableColumnCellHeader;
