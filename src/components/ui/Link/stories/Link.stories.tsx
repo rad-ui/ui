886fdfea7bf63c1a8f55e0e75939720a209e4fa2
jindaljyoti <137828895+jindaljyoti@users.noreply.github.com>
2025-03-22 19:08:45 -0700

    add sizes for link component (#949)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- src/components/ui/Link/stories/Link.stories.tsx
+++ src/components/ui/Link/stories/Link.stories.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Link, { LinkProps } from '../Link';
 import SandboxEditor from '~/components/tools/SandboxEditor/SandboxEditor';
 
+const Sizes = ['small', 'medium', 'large', 'x-large']
+
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
     title: 'Components/Link',
@@ -20,4 +22,20 @@ export const All = {
         href: 'https://www.google.com',
         target: '_blank'
     }
+};
+
+export const Size = () => {
+    return <SandboxEditor>
+        <div className='mt-4 mb-2'>
+            <p className='text-gray-950'>Link Size</p>
+        </div>
+        <div>
+              <div className='flex flex-col'>
+                        {Sizes.map((size, index) => {
+                            return <Link key={index} href= 'https://www.google.com' size={size}>Hello</Link>;
+                        })}
+               </div>
+
+        </div>
+    </SandboxEditor>;
 };
