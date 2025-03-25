48b8409a34f1b750109558fb98b9621b985d3b2a
jyoti <jindaljyoti913@gmail.com>
2025-03-25 12:14:04 -0700

    add size for separotor component

--- src/components/ui/Separator/stories/Separator.stories.tsx
+++ src/components/ui/Separator/stories/Separator.stories.tsx
@@ -3,6 +3,8 @@ import Separator, { SeparatorProps } from '../Separator';
 import SandboxEditor from '~/components/tools/SandboxEditor/SandboxEditor';
 
 const textClasses = 'text-gray-950 text-sm font-light';
+const Sizes = ['small', 'medium', 'large', 'x-large'];
+
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
     title: 'Components/Separator',
@@ -33,4 +35,21 @@ export const Color = {
     args: {
         color: 'blue'
     }
+};
+
+export const Size = () => {
+    return <SandboxEditor>
+        <div className='mt-4 mb-2'>
+            <p className='text-gray-950'>Separator Size</p>
+        </div>
+        <div>
+            <div className='flex flex-col'>
+                {Sizes.map((size, index) => {
+                    return <div>
+                          <Separator key={index} size={size} />
+                          </div>
+                })}
+            </div>
+       </div>
+    </SandboxEditor>;
 };
