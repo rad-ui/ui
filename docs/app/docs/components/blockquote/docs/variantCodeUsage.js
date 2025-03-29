9c347aca2268baa38043464f219700b8b25f4ce6
jindaljyoti <137828895+jindaljyoti@users.noreply.github.com>
2025-03-28 23:19:53 -0700

    delete variants and fix dark mode (#996)

--- docs/app/docs/components/blockquote/docs/variantCodeUsage.js
+++
@@ -1,9 +0,0 @@
-import { getSourceCodeFromPath } from '@/utils/parseSourceCode';
-
-const blockQuoteVariantsSourceCode = await getSourceCodeFromPath('docs/app/docs/components/blockquote/examples/BlockQuoteVariants.tsx');
-
-export const code = {
-    javascript: {
-        code: blockQuoteVariantsSourceCode
-    }
-};
