bda6ea58d74d1ea32344d4e20bb601af00f6da44
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-04-03 17:36:51 +0530

    Delete unused Popper component and context files

--- src/components/tools/Popper/context/usePopper.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import PopperContext from './PopperContext';
-
-export default function usePopper() {
-    const ctx = useContext(PopperContext);
-    if (ctx == null) throw new Error('Popper connot be used outside of popper context provider');
-
-    return ctx;
-}
