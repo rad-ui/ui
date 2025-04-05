ccf39c1f78974c9743c217eb2d52636ead15c32e
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-04-05 15:38:55 +0530

    Add API documentation for BlockQuote component

---
+++ docs/app/docs/components/blockquote/docs/seo.js
@@ -0,0 +1,17 @@
+export default {
+  title: 'BlockQuote Component',
+  description: 'A versatile blockquote component for displaying quoted content with different sizes and color options',
+  keywords: [
+    'blockquote',
+    'quote',
+    'citation',
+    'testimonial',
+    'text formatting',
+    'content emphasis',
+    'ui component',
+    'sizes',
+    'colors',
+    'react',
+    'radui'
+  ],
+} 
