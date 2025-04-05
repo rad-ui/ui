38c846fbb0ba3a099bbf88f78e77c53b6576fd63
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-04-05 09:06:00 +0530

    Improve Accordion SEO (#1028)

---
+++ docs/app/docs/components/accordion/docs/seo/index.js
@@ -0,0 +1,30 @@
+const metadata = {
+    title: 'Accordion | Rad UI',
+    description: 'Accordions are a common UX pattern that can help reduce the amount of information presented to users. They are used to toggle the visibility of content, condensing information and reducing clutter.',
+    keywords: 'accordion, react component, UI component, collapsible content, expandable panels, rad ui, accessibility, a11y, keyboard navigation, disclosure component',
+    authors: [{ name: 'Rad UI Team' }, { name: 'Pranay Kothapalli' }],
+    // openGraph: {
+    //   type: 'website',
+    //   title: 'Accordion Component | Rad UI',
+    //   description: 'An accessible, customizable accordion component for React applications. Toggle content visibility, reduce clutter, and improve user experience.',
+    //   images: [
+    //     {
+    //       url: '/images/components/accordion-og.png',
+    //       width: 1200,
+    //       height: 630,
+    //       alt: 'Rad UI Accordion Component',
+    //     }
+    //   ],
+    // },
+    // twitter: {
+    //   card: 'summary_large_image',
+    //   title: 'Accordion Component | Rad UI',
+    //   description: 'An accessible, customizable accordion component for React applications. Toggle content visibility, reduce clutter, and improve user experience.',
+    //   images: ['/images/components/accordion-og.png'],
+    // },
+    alternates: {
+      canonical: 'https://rad-ui.com/docs/components/accordion',
+    }
+}
+
+export default metadata;
--- docs/components/ui/Tooltip/index.tsx
+++ docs/components/ui/Tooltip/index.tsx
@@ -1,9 +1,9 @@
 "use client"
 import Tooltip from "@radui/ui/Tooltip"
 
-const TooltipWrapper = ({ children, label='', ...props }: { children: React.ReactNode, label: string, props: any }) => {
+const TooltipWrapper = ({ children, label='', placement='bottom', ...props }: { children: React.ReactNode, label: string, placement: string, props?: any }) => {
     return (
-        <Tooltip.Root {...props}>
+        <Tooltip.Root {...props} placement={placement}>
             <Tooltip.Trigger asChild>
                 {children}
             </Tooltip.Trigger>
