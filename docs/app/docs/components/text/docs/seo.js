6df9c4c25e6c8287c0c995fadfda756eb6b0bf05
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-04-05 15:22:42 +0530

    Add Text component API documentation and features

---
+++ docs/app/docs/components/text/docs/seo.js
@@ -0,0 +1,15 @@
+export default {
+  title: 'Text Component',
+  description: 'A flexible component for displaying text content with customizable styling',
+  keywords: [
+    'text',
+    'typography',
+    'paragraph',
+    'span',
+    'label',
+    'div',
+    'ui component',
+    'react',
+    'radui'
+  ],
+} 
