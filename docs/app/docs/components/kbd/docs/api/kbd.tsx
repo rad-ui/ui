e006de28215701673cfd5a6bfbd23f22591b93ca
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-04-05 15:41:41 +0530

    Add Kbd component API documentation and features

---
+++ docs/app/docs/components/kbd/docs/api/kbd.tsx
@@ -0,0 +1,58 @@
+const data = {
+    name: "Kbd",
+    description: "A component that visually represents keyboard buttons or key combinations, often used in documentation to indicate keyboard shortcuts or commands.",
+    columns: [
+        {
+            name: "Prop",
+            id: "prop",
+        },
+        {
+            name: "Type",
+            id: "type",
+        },
+        {
+            name: "Default",
+            id: "default",
+        },
+        {
+            name: "",
+            id: "info_tooltips",
+        }
+    ],
+    data: [
+        {
+            prop: {
+                name: "children",
+                info_tooltips: "The content to be displayed inside the keyboard key element."
+            },
+            type: "ReactNode",
+            default: "--",
+        },
+        {
+            prop: {
+                name: "className",
+                info_tooltips: "Additional CSS class names to apply to the kbd element."
+            },
+            type: "string",
+            default: "''",
+        },
+        {
+            prop: {
+                name: "customRootClass",
+                info_tooltips: "Custom root class name to override default styling."
+            },
+            type: "string",
+            default: "''",
+        },
+        {
+            prop: {
+                name: "size",
+                info_tooltips: "Size variant of the keyboard key."
+            },
+            type: "string",
+            default: "''",
+        }
+    ]
+};
+
+export default data; 
