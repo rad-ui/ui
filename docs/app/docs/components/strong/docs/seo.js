11319fb94d8d59f67f0e05e71ab7c6394d27af43
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-04-05 15:52:34 +0530

    Add Strong component API documentation and features

---
+++ docs/app/docs/components/strong/docs/seo.js
@@ -0,0 +1,17 @@
+export default {
+  title: 'Strong Component',
+  description: 'A component for highlighting important text with semantic HTML strong element',
+  keywords: [
+    'strong',
+    'bold',
+    'importance',
+    'emphasis',
+    'text formatting',
+    'semantic HTML',
+    'typography',
+    'ui component',
+    'accessibility',
+    'react',
+    'radui'
+  ],
+} 
