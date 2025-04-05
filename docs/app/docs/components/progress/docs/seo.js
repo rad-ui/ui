846205e78db1b23a1003619942225b247fb6551e
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-04-05 15:43:55 +0530

    Add Progress component API documentation

---
+++ docs/app/docs/components/progress/docs/seo.js
@@ -0,0 +1,19 @@
+export default {
+  title: 'Progress Component',
+  description: 'A component for displaying the completion status of tasks, uploads, or other processes',
+  keywords: [
+    'progress',
+    'progress bar',
+    'loading',
+    'indicator',
+    'status',
+    'completion',
+    'upload',
+    'download',
+    'process',
+    'ui component',
+    'accessibility',
+    'react',
+    'radui'
+  ],
+} 
