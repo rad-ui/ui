f05bcf6d5097361f29611d8e7aa24ca47833fe60
jindaljyoti <137828895+jindaljyoti@users.noreply.github.com>
2025-04-07 22:21:57 -0700

    size and variant for card component (#1040)

--- src/components/ui/Card/fragments/CardRoot.tsx
+++ src/components/ui/Card/fragments/CardRoot.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { customClassSwitcher } from '~/core';
 import { clsx } from 'clsx';
+import { useCreateDataAttribute } from '~/core/hooks/createDataAttribute';
 const COMPONENT_NAME = 'Card';
 export type CardRootProps = {
     children: React.ReactNode;
     customRootClass?: string;
+    variant?: string;
+    size?: string;
     className?: string;
     props?: any;
 };
 
-const CardRoot = ({ children, customRootClass, className = '', ...props }: CardRootProps) => {
+const CardRoot = ({ children, customRootClass, className = '', variant = '', size = '', ...props }: CardRootProps) => {
     const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME);
+    const dataAttributes = useCreateDataAttribute('card', { variant, size });
 
     return (
-        <div className={clsx(rootClass, className)} {...props} >
+        <div className={clsx(rootClass, className)} {...dataAttributes()} {...props} >
             {children}
         </div>
     );
