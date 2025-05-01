4d4cc55f344f0f04fe3fc3be28fa7b951e473fe9
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-05-01 21:19:04 +0530

    Refactor Badge component structure and props handling

--- src/components/ui/Badge/Badge.tsx
+++ src/components/ui/Badge/Badge.tsx
@@ -1,8 +1,10 @@
 'use client';
 import React from 'react';
-import BadgeRoot from './fragments/BadgeRoot';
-import BadgeContent from './fragments/BadgeContent';
 
+import { customClassSwitcher } from '~/core';
+import { clsx } from 'clsx';
+
+const COMPONENT_NAME = 'Badge';
 export type BadgeProps = {
     children?: React.ReactNode,
     customRootClass?: string,
@@ -14,13 +16,24 @@ export type BadgeProps = {
 }
 
 const Badge = ({ children, customRootClass = '', className = '', color = '', variant = '', size = '', ...props }: BadgeProps) => {
-    return <BadgeRoot customRootClass={customRootClass} className={className} color={color} variant={variant} size={size} {...props}>
-        <BadgeContent>
-            {children}
-        </BadgeContent>
-    </BadgeRoot>;
+    const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME);
+    const data_attributes: Record<string, string> = {};
+
+    if (variant) {
+        data_attributes['data-badge-variant'] = variant;
+    }
+
+    if (size) {
+        data_attributes['data-badge-size'] = size;
+    }
+
+    if (color) {
+        data_attributes['data-rad-ui-accent-color'] = color;
+    }
+
+    return <div className={clsx(rootClass, className)} {...data_attributes} {...props}>
+        {children}
+    </div>;
 };
 
-Badge.Root = BadgeRoot;
-Badge.Content = BadgeContent;
 export default Badge;
