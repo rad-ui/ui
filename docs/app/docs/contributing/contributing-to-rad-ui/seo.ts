e77f6ea4b0c151a50e2b6cf47894781067e29724
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-05-01 16:06:41 +0530

    Draft - Better SEO descriptions (#1064)

---
+++ docs/app/docs/contributing/contributing-to-rad-ui/seo.ts
@@ -0,0 +1,8 @@
+import generateSeoMetadata from "@/utils/seo/generateSeoMetadata"
+
+const contributingToRadUiMetadata = generateSeoMetadata({
+    title: "Contributing to Rad UI",
+    description: "Contributing to Rad UI - an Open Source React Headless UI Library",
+})
+
+export default contributingToRadUiMetadata
