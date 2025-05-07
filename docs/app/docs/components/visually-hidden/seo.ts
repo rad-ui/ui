8f95673ad146767c11bc9307a00f685e40df263c
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-05-07 11:14:08 +0530

    fix: seo

--- docs/app/docs/components/visually-hidden/seo.ts
+++ docs/app/docs/components/visually-hidden/seo.ts
@@ -2,7 +2,8 @@ import generateSeoMetadata from "@/utils/seo/generateSeoMetadata"
 
 const visuallyHiddenMetadata = generateSeoMetadata({
     title: "Visually Hidden - Rad UI",
-    description: "A headless React Visually Hidden component for creating accessible, customizable visually hidden content. Ideal for improving UX in user interfaces."
+    description: "A headless React Visually Hidden component for creating accessible, customizable hidden content. Perfect for enhancing UX by improving accessibility for screen readers without affecting visual design."
 });
+
 
 export default visuallyHiddenMetadata
