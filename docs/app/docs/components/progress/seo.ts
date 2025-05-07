3de40becc48ecc00fb0bb9d28f5161f31794cf6f
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-05-07 11:09:37 +0530

    chore: improve seo for pages

---
+++ docs/app/docs/components/progress/seo.ts
@@ -0,0 +1,9 @@
+import generateSeoMetadata from "@/utils/seo/generateSeoMetadata"
+
+const progressMetadata = generateSeoMetadata({
+    title: "Progress - Rad UI",
+    description: "A headless React Progress component for customizable progress bars with built-in accessibility features. Ideal for displaying task completion, loading indicators, and visual progress in user interfaces."
+});
+
+
+export default progressMetadata
---
+++ docs/app/docs/components/visually-hidden/seo.ts
@@ -0,0 +1,8 @@
+import generateSeoMetadata from "@/utils/seo/generateSeoMetadata"
+
+const visuallyHiddenMetadata = generateSeoMetadata({
+    title: "Visually Hidden - Rad UI",
+    description: "A headless React Visually Hidden component for creating accessible, customizable visually hidden content. Ideal for improving UX in user interfaces."
+});
+
+export default visuallyHiddenMetadata
