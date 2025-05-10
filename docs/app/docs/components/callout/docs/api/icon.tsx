1a94b0d62a2e86c16330f40b904b98e3632cc47b
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-05-10 18:56:23 +0530

    add: callout docs

---
+++ docs/app/docs/components/callout/docs/api/icon.tsx
@@ -0,0 +1,38 @@
+const data = {
+    name : "Icon",
+    description : "The icon component for the Callout. This component is used to display the icon content of the Callout.",
+    columns : [
+        {
+            name : "Prop",
+            id : "prop",
+        },
+        {
+            name : "Type",
+            id : "type",
+        },
+        {
+            name: "Default",
+            id : "default",
+        }
+    ],
+    data:[
+       {
+        prop : {
+            name : "children",
+            info_tooltips : "The content of the CalloutIcon."
+        },
+        type : "ReactNode",
+        default : "--",
+       },
+       {
+        prop: {
+            name : "className",
+            info_tooltips: 'The class name for the CalloutIcon.'
+        },
+        type: 'string',
+        default: '--',
+       }
+    ]
+}
+
+export default data;
---
+++ docs/app/docs/components/callout/docs/api/root.tsx
@@ -0,0 +1,76 @@
+const data = {
+    name : "Trigger",
+    description : "The trigger component for the Accordion. This component is used to toggle the visibility of the AccordionItem.",
+    columns : [
+        {
+            name : "Prop",
+            id : "prop",
+        },
+        {
+            name : "Type",
+            id : "type",
+        },
+        {
+            name: "Default",
+            id : "default",
+        }
+    ],
+    data:[
+       {
+        prop : {
+            name : "children",
+            info_tooltips : "The content of the CalloutRoot."
+        },
+        type : "ReactNode",
+        default : "--",
+       },
+       {
+        prop: {
+            name : "asChild",
+            info_tooltips: 'Whether to use the child component as the AccordionTrigger.'
+        },
+        type: 'boolean',
+        default: 'false',
+       },
+       {
+        prop: {
+            name : "className",
+            info_tooltips: 'The class name for the CalloutRoot.'
+        },
+       },
+       {
+        prop: {
+            name : "customRootClass",
+            info_tooltips: 'The custom class name for the CalloutRoot.'
+        },
+        type: 'string',
+        default: '--',
+       },
+       {
+        prop: {
+            name : "color",
+            info_tooltips: 'The color of the CalloutRoot.'
+        },
+        type: 'string',
+        default: '--',
+       },
+       {
+        prop: {
+            name : "variant",
+            info_tooltips: 'The variant of the CalloutRoot.'
+        },
+        type: 'string',
+        default: '--',
+       },
+       {
+        prop: {
+            name : "size",
+            info_tooltips: 'The size of the CalloutRoot.'
+        },
+        type: 'string',
+        default: '--',
+       }
+    ]
+}
+
+export default data;
