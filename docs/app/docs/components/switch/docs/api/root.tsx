754c267eae910e2909ed51bd1bb45916f20a51c8
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-05-10 20:49:37 +0530

    add: switch and toggle group

---
+++ docs/app/docs/components/switch/docs/api/root.tsx
@@ -0,0 +1,38 @@
+const data = {
+    name: "Root",
+    description: "Root component for the Switch component.",
+    columns: [
+        {
+            name: "Prop",
+            id: "prop",
+        },
+        {
+            name: "Type",
+            id: "type",
+        },
+        {
+            name: "Default",
+            id: "default",
+        }
+    ],
+    data: [
+        {
+            prop: {
+                name: "children",
+                info_tooltips: "The children of the component."
+            },
+            type: "React.ReactNode",
+            default: "null",
+        },
+        {
+            prop: {
+                name: "customRootClass",
+                info_tooltips: "Custom root class name to override default styling."
+            },
+            type: "string",
+            default: "''",
+        }
+    ]
+};
+
+export default data; 
