631c3983bacad5cea631d40f788e5315d886f707
GoldGroove06 <73437174+GoldGroove06@users.noreply.github.com>
2025-07-21 13:05:56 +0530

    fixing the broken build due to conflict resolving

--- src/core/primitives/RadioGroup/fragments/RadioGroupPrimitiveRoot.tsx
+++ src/core/primitives/RadioGroup/fragments/RadioGroupPrimitiveRoot.tsx
@@ -29,6 +29,7 @@ const RadioGroupPrimitiveRoot = ({ value, defaultValue = '', onValueChange, chil
         selectedValue,
         setSelectedValue,
         groupDisabled
+    };
 
     return (
         <Primitive.div {...props} aria-required={required} role='radiogroup' aria-disabled={groupDisabled}>
