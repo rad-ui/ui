5b3a0573b567b11885d1b57325383294f29b731f
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-08-02 21:41:41 +0530

    Improvements and fixes - 2 Aug 2025 batch (#1286)

--- src/components/ui/Progress/fragments/ProgressRoot.tsx
+++ src/components/ui/Progress/fragments/ProgressRoot.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { clsx } from 'clsx';
 import { customClassSwitcher } from '~/core';
 import { ProgressContext } from '../contexts/ProgressContext';
@@ -9,25 +9,56 @@ import Primitive from '~/core/primitives/Primitive';
 const COMPONENT_NAME = 'Progress';
 
 type ProgressRootProps = {
-    value: number;
+    asChild?: boolean;
+    value: number | null;
     minValue: number;
     maxValue: number;
+    getValueLabel?: (value: number, minValue: number, maxValue: number) => string;
     children: React.ReactNode;
     customRootClass?: string;
 }
 
-const ProgressRoot = ({ value = 0, minValue = 0, maxValue = 100, children, customRootClass }: ProgressRootProps) => {
+const STATE_ENUMS = {
+    LOADING: 'loading', // a progress is loading when the value is not null and not equal to the maxValue
+    COMPLETE: 'complete', // a progress is complete when the value is equal to the maxValue
+    INDETERMINATE: 'indeterminate' // a progress is indeterminate when the value is null, by default it's 0
+} as const;
+
+const ProgressRoot = ({ value = 0, minValue = 0, maxValue = 100, children, customRootClass, asChild, getValueLabel }: ProgressRootProps) => {
+    const [state, setState] = useState<typeof STATE_ENUMS[keyof typeof STATE_ENUMS]>(STATE_ENUMS.LOADING);
+
     const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME);
+    const ariaLabel = getValueLabel?.(value ?? 0, minValue, maxValue) ?? '';
+
+    useEffect(() => {
+        setState(value === null ? STATE_ENUMS.INDETERMINATE : value === maxValue ? STATE_ENUMS.COMPLETE : STATE_ENUMS.LOADING);
+    }, [value, maxValue]);
+
     const sendValues = {
         value,
         minValue,
         maxValue,
-        rootClass
+        rootClass,
+        getValueLabel,
+        ariaLabel,
+        state
     };
-
     return (
         <ProgressContext.Provider value={sendValues}>
-            <Primitive.div className={clsx(rootClass)}>{children}</Primitive.div>
+            <Primitive.div
+                role="progressbar"
+                aria-label={ariaLabel}
+                aria-valuetext={ariaLabel}
+                aria-valuenow={value ?? 0}
+                aria-valuemin={minValue}
+                aria-valuemax={maxValue}
+                data-state={state}
+                data-value={value ?? 0}
+                data-max={maxValue}
+                data-min={minValue}
+                className={clsx(rootClass)}
+                asChild={asChild}
+            >{children}</Primitive.div>
         </ProgressContext.Provider>
     );
 };
