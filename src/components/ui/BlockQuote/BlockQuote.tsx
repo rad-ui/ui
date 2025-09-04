e2a420e788e54cc25fdaec9dbea41f5d7617a9ce
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-09-04 18:26:44 +0530

    refactor(BlockQuote): forward ref support (#1408)

--- src/components/ui/BlockQuote/BlockQuote.tsx
+++ src/components/ui/BlockQuote/BlockQuote.tsx
@@ -5,34 +5,38 @@ import { customClassSwitcher } from '~/core';
 
 const COMPONENT_NAME = 'BlockQuote';
 
-export type BlockQuoteProps = {
-    children: React.ReactNode;
+export type BlockQuoteProps = React.ComponentPropsWithoutRef<'blockquote'> & {
     customRootClass?: string;
-    className?: string;
     color?: string;
     variant?: string;
     size?: string;
-    props?: Record<string, any>[]
-}
-const BlockQuote = ({ children, customRootClass = '', className = '', color = '', variant = '', size = '', ...props }: BlockQuoteProps) => {
-    const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME);
-
-    const data_attributes: Record<string, string> = {};
-
-    if (variant) {
-        data_attributes['data-block-quote-variant'] = variant;
-    }
-
-    if (size) {
-        data_attributes['data-block-quote-size'] = size;
-    }
-
-    if (color) {
-        data_attributes['data-rad-ui-accent-color'] = color;
-    }
-
-    return <blockquote className={clsx(rootClass, className)} {...props} {...data_attributes}> {children}</blockquote>;
 };
+
+const BlockQuote = React.forwardRef<React.ElementRef<'blockquote'>, BlockQuoteProps>(
+    ({ children, customRootClass = '', className = '', color = '', variant = '', size = '', ...props }, ref) => {
+        const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME);
+
+        const data_attributes: Record<string, string> = {};
+
+        if (variant) {
+            data_attributes['data-block-quote-variant'] = variant;
+        }
+
+        if (size) {
+            data_attributes['data-block-quote-size'] = size;
+        }
+
+        if (color) {
+            data_attributes['data-rad-ui-accent-color'] = color;
+        }
+
+        return (
+            <blockquote ref={ref} className={clsx(rootClass, className)} {...props} {...data_attributes}>
+                {children}
+            </blockquote>
+        );
+    }
+);
 
 BlockQuote.displayName = COMPONENT_NAME;
 
