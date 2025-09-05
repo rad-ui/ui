36dbdc893417e5b584f483da9c39f1fa371b1758
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-09-05 08:20:36 +0530

    refactor slider with forward refs

--- src/components/ui/Slider/Slider.tsx
+++ src/components/ui/Slider/Slider.tsx
@@ -1,14 +1,27 @@
 'use client';
 
+import React, { forwardRef, ElementRef, ComponentPropsWithoutRef, ForwardRefExoticComponent, RefAttributes } from 'react';
 import SliderRoot from './fragments/SliderRoot';
 import SliderTrack from './fragments/SliderTrack';
 import SliderThumb from './fragments/SliderThumb';
 import SliderRange from './fragments/SliderRange';
 
-const Slider = () => {
+export type SliderElement = ElementRef<'div'>;
+export type SliderProps = ComponentPropsWithoutRef<'div'>;
+
+type SliderComponent = ForwardRefExoticComponent<SliderProps & RefAttributes<SliderElement>> & {
+    Root: typeof SliderRoot;
+    Track: typeof SliderTrack;
+    Range: typeof SliderRange;
+    Thumb: typeof SliderThumb;
+};
+
+const Slider = forwardRef<SliderElement, SliderProps>((_props, _ref) => {
     console.warn('Direct usage of Slider is not supported. Please use Slider.Root, Slider.Track, etc. instead.');
     return null;
-};
+}) as SliderComponent;
+
+Slider.displayName = 'Slider';
 
 Slider.Root = SliderRoot;
 Slider.Track = SliderTrack;
