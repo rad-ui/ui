775e04c1bf4799dcab154d308fdea4a9bb391933
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-09-05 12:45:02 +0530

    refactor: forward refs in Table components (#1460)

--- src/components/ui/Table/fragments/TableColumnCellHeader.tsx
+++ src/components/ui/Table/fragments/TableColumnCellHeader.tsx
@@ -4,11 +4,18 @@ import { clsx } from 'clsx';
 
 const COMPONENT_NAME = 'TableColumnCellHeader';
 
-const TableColumnCellHeader = ({ children, className = 'cell-header', ...props }:any) => {
-    return <th className={clsx(className)} {...props}>
-        {children}
-    </th>;
-};
+type TableColumnCellHeaderProps = React.ComponentPropsWithoutRef<'th'>;
+
+const TableColumnCellHeader = React.forwardRef<
+    React.ElementRef<'th'>,
+    TableColumnCellHeaderProps
+>(({ children, className = 'cell-header', ...props }, ref) => {
+    return (
+        <th ref={ref} className={clsx(className)} {...props}>
+            {children}
+        </th>
+    );
+});
 
 TableColumnCellHeader.displayName = COMPONENT_NAME;
 
--- src/components/ui/Table/fragments/TableRow.tsx
+++ src/components/ui/Table/fragments/TableRow.tsx
@@ -3,11 +3,18 @@ import React from 'react';
 import { clsx } from 'clsx';
 
 const COMPONENT_NAME = 'TableRow';
-const TableRow = ({ children, className = 'row', ...props }:any) => {
-    return <tr className={clsx(className)} {...props} >
-        {children}
-    </tr>;
-};
+
+type TableRowProps = React.ComponentPropsWithoutRef<'tr'>;
+
+const TableRow = React.forwardRef<React.ElementRef<'tr'>, TableRowProps>(
+    ({ children, className = 'row', ...props }, ref) => {
+        return (
+            <tr ref={ref} className={clsx(className)} {...props}>
+                {children}
+            </tr>
+        );
+    }
+);
 
 TableRow.displayName = COMPONENT_NAME;
 
