dd4191d60203c581900af8518be9f99aa1e23c17
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-09-05 07:31:04 +0530

    refactor: forwardRef Accordion component (#1421)

--- src/components/ui/Accordion/fragments/AccordionContent.tsx
+++ src/components/ui/Accordion/fragments/AccordionContent.tsx
@@ -4,26 +4,28 @@ import React, { useContext } from 'react';
 import { AccordionContext } from '../contexts/AccordionContext';
 import CollapsiblePrimitive from '~/core/primitives/Collapsible';
 
-type AccordionContentProps = {
-  children: React.ReactNode;
+type AccordionContentProps = React.ComponentPropsWithoutRef<'div'> & {
   index: number,
-  className? :string
 };
 
-const AccordionContent: React.FC<AccordionContentProps> = ({ children, index, className = '' }: AccordionContentProps) => {
+const AccordionContent = React.forwardRef<React.ElementRef<'div'>, AccordionContentProps>(({ children, index, className = '', ...props }, ref) => {
     const { rootClass } = useContext(AccordionContext);
 
     return (
         <CollapsiblePrimitive.Content
+            ref={ref}
             asChild
             className={clsx(`${rootClass}-content`, className)}
             id={`content-${index}`}
             role="region"
             aria-labelledby={`section-${index}`}
+            {...props}
         >
             {children}
         </CollapsiblePrimitive.Content>
     );
-};
+});
+
+AccordionContent.displayName = 'AccordionContent';
 
 export default AccordionContent;
