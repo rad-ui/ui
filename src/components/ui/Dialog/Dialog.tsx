bb0a63d5b5de1e459621bfb8de08f2f44b6f6944
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-09-05 12:58:04 +0530

    refactor(dialog): forward refs and add tests (#1464)

--- src/components/ui/Dialog/Dialog.tsx
+++ src/components/ui/Dialog/Dialog.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import React, { forwardRef } from 'react';
 import DialogRoot from './fragments/DialogRoot';
 import DialogTrigger from './fragments/DialogTrigger';
 import DialogPortal from './fragments/DialogPortal';
@@ -9,10 +10,28 @@ import DialogTitle from './fragments/DialogTitle';
 import DialogDescription from './fragments/DialogDescription';
 import DialogClose from './fragments/DialogClose';
 
-const Dialog = () => {
+type DialogElement = React.ElementRef<'div'>;
+type DialogProps = React.ComponentPropsWithoutRef<'div'>;
+
+const DialogBase = forwardRef<DialogElement, DialogProps>((_props, _ref) => {
     console.warn('Direct usage of Dialog is not supported. Please use Dialog.Root, Dialog.Content, etc. instead.');
     return null;
-};
+});
+
+DialogBase.displayName = 'Dialog';
+
+interface DialogComponent extends React.ForwardRefExoticComponent<DialogProps & React.RefAttributes<DialogElement>> {
+    Root: typeof DialogRoot;
+    Trigger: typeof DialogTrigger;
+    Portal: typeof DialogPortal;
+    Overlay: typeof DialogOverlay;
+    Content: typeof DialogContent;
+    Title: typeof DialogTitle;
+    Description: typeof DialogDescription;
+    Close: typeof DialogClose;
+}
+
+const Dialog = DialogBase as DialogComponent;
 
 Dialog.Root = DialogRoot;
 Dialog.Trigger = DialogTrigger;
