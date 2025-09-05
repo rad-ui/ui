7ef32bfd7f41918fe4fc713fd454cfa79b78ebe9
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-09-05 07:40:00 +0530

    refactor: forwardRef Disclosure components

--- src/components/ui/Disclosure/Disclosure.tsx
+++ src/components/ui/Disclosure/Disclosure.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import DisclosureRoot from './fragments/DisclosureRoot';
+import DisclosureRoot, { DisclosureRootProps } from './fragments/DisclosureRoot';
 import DisclosureItem from './fragments/DisclosureItem';
 import DisclosureTrigger from './fragments/DisclosureTrigger';
 import DisclosureContent from './fragments/DisclosureContent';
 
-export type DisclosureProps = {
-
+export type DisclosureProps = DisclosureRootProps & {
      items:{title:string, content: React.ReactNode}[]
+};
+
+interface DisclosureComponent extends React.ForwardRefExoticComponent<DisclosureProps & React.RefAttributes<React.ElementRef<'div'>>> {
+    Root: typeof DisclosureRoot;
+    Item: typeof DisclosureItem;
+    Trigger: typeof DisclosureTrigger;
+    Content: typeof DisclosureContent;
 }
 
-const Disclosure = ({ items }:DisclosureProps) => {
+const Disclosure = React.forwardRef<React.ElementRef<'div'>, DisclosureProps>(({ items, ...props }, ref) => {
     return (
 
-        <DisclosureRoot>
+        <DisclosureRoot ref={ref} {...props}>
             {items.map((item, index) => (
                 <DisclosureItem key={index} value={index}>
                     <DisclosureTrigger>
@@ -27,11 +33,13 @@ const Disclosure = ({ items }:DisclosureProps) => {
 
         </DisclosureRoot>
     );
-};
+}) as DisclosureComponent;
 
 Disclosure.Root = DisclosureRoot;
 Disclosure.Item = DisclosureItem;
 Disclosure.Trigger = DisclosureTrigger;
 Disclosure.Content = DisclosureContent;
+
+Disclosure.displayName = 'Disclosure';
 
 export default Disclosure;
