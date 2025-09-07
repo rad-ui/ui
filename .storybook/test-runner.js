7553e764851ae89c6ce2874fb2d96d5e27302a2a
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-09-07 09:20:27 +0530

    ci: run storybook smoke tests

---
+++ .storybook/test-runner.js
@@ -0,0 +1,11 @@
+import { within, userEvent } from '@storybook/testing-library';
+
+// Basic smoke test for each story. If a button exists, click it to ensure interactions
+// don't trigger runtime errors.
+export const run = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  const button = canvas.queryByRole('button');
+  if (button) {
+    await userEvent.click(button);
+  }
+};
