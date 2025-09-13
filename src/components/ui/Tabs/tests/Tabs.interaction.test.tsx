7c5e81949806f9c6ea5d6f8f213011eb1649e87d
Pranay Kothapalli <pranaykothapalli@gmail.com>
2025-09-13 13:48:43 +0530

    Fix TextEncoder polyfill issue in Tabs.interaction.test.tsx
    - Replace ES6 imports with require() for react-dom/server and react-dom/client
    - Use consistent pattern with other SSR test files
    - Resolves ReferenceError: TextEncoder is not defined

--- src/components/ui/Tabs/tests/Tabs.interaction.test.tsx
+++ src/components/ui/Tabs/tests/Tabs.interaction.test.tsx
@@ -5,17 +5,12 @@ import Tabs from '../Tabs';
 import * as axe from 'axe-core';
 import { ACCESSIBILITY_TEST_TAGS } from '~/setupTests';
 import { TextEncoder, TextDecoder } from 'util';
-
-// @ts-ignore - react-dom/client types not available
-import { hydrateRoot } from 'react-dom/client';
-// @ts-ignore - react-dom/server types not available
-import { renderToString } from 'react-dom/server';
-
-// Polyfill TextEncoder/TextDecoder for server rendering in Jest
-// @ts-ignore
-if (!global.TextEncoder) global.TextEncoder = TextEncoder;
-// @ts-ignore
-if (!global.TextDecoder) global.TextDecoder = TextDecoder;
+(global as any).TextEncoder = TextEncoder;
+(global as any).TextDecoder = TextDecoder;
+// @ts-ignore - React 18/19 server rendering types may not be present
+const { renderToString } = require('react-dom/server');
+// @ts-ignore - hydrateRoot typings may be unavailable in this environment
+const { hydrateRoot } = require('react-dom/client');
 
 // Helper to wait for axe asynchronously
 const runAxe = (container: HTMLElement) =>
