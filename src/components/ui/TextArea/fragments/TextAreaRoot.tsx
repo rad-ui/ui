ca9a057eba8cd47bc9adfee6d50bd18dbe766e99
GoldGroove06 <73437174+GoldGroove06@users.noreply.github.com>
2025-09-26 23:42:56 +0530

    add: api textarea

--- src/components/ui/TextArea/fragments/TextAreaRoot.tsx
+++ src/components/ui/TextArea/fragments/TextAreaRoot.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { customClassSwitcher } from '~/core';
 import clsx from 'clsx';
+import { useCreateDataAttribute } from '~/core/hooks/createDataAttribute';
 
 const COMPONENT_NAME = 'TextArea';
 
 export type TextAreaRootProps = React.ComponentPropsWithoutRef<'div'> & {
     customRootClass?: string;
+    variant?: string;
+    size?: string;
+    resize?: 'none' | 'vertical' | 'horizontal' | 'both';
 };
 
 const TextAreaRoot = React.forwardRef<React.ElementRef<'div'>, TextAreaRootProps>(
-    ({ children, customRootClass = '', className = '', ...props }, ref) => {
+    ({ children, customRootClass = '', className = '', variant = '', size = '', resize = 'both', ...props }, ref) => {
         const rootClass = customClassSwitcher(customRootClass, COMPONENT_NAME);
+            const dataAttributes = useCreateDataAttribute('textarea', { variant, size, resize });
 
         return (
-            <div ref={ref} className={clsx(rootClass, className)} {...props}>
+            <div ref={ref} className={clsx(rootClass, className)} {...props} {...dataAttributes()}>
                 {children}
             </div>
         );
