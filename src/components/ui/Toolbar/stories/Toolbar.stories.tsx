046a1ab1c12595b36906e51728d1f8fc93050df6
Pranay Kothapalli <pranaykothapalli@gmail.com>
2026-02-20 10:56:40 +0530

    chore(toolbar): refine story styles and link semantics

--- src/components/ui/Toolbar/stories/Toolbar.stories.tsx
+++ src/components/ui/Toolbar/stories/Toolbar.stories.tsx
@@ -14,38 +14,52 @@ const Template = (_args: any) => {
   const [alignment, setAlignment] = React.useState<string[]>(['left']);
 
   return (
-    <SandboxEditor className="space-y-4 pt-4">
-      <Toolbar.Root aria-label="Formatting options">
-        <Toolbar.ToggleGroup type="single" value={alignment} onValueChange={setAlignment}>
-          <Toolbar.ToggleItem value="left" aria-label="Align left">
-            <AlignLeft size={14} />
-          </Toolbar.ToggleItem>
-          <Toolbar.ToggleItem value="center" aria-label="Align center">
-            <AlignCenter size={14} />
-          </Toolbar.ToggleItem>
-          <Toolbar.ToggleItem value="right" aria-label="Align right">
-            <AlignRight size={14} />
-          </Toolbar.ToggleItem>
-        </Toolbar.ToggleGroup>
-        <Toolbar.Separator />
-        <Toolbar.ToggleGroup type="multiple" value={textStyles} onValueChange={setTextStyles}>
-          <Toolbar.ToggleItem value="bold" aria-label="Bold">
-            <Bold size={14} />
-          </Toolbar.ToggleItem>
-          <Toolbar.ToggleItem value="italic" aria-label="Italic">
-            <Italic size={14} />
-          </Toolbar.ToggleItem>
-          <Toolbar.ToggleItem value="underline" aria-label="Underline">
-            <Underline size={14} />
-          </Toolbar.ToggleItem>
-        </Toolbar.ToggleGroup>
-        <Toolbar.Separator />
-        <Toolbar.Link href="#" aria-label="Insert link">
-          <Link2 size={14} />
-        </Toolbar.Link>
-      </Toolbar.Root>
-      <div className="text-sm text-gray-700">
-        Active styles: {textStyles.join(', ') || 'none'} | Alignment: {alignment[0] ?? 'none'}
+    <SandboxEditor className="space-y-6 pt-4">
+      <div
+        style={{
+          minHeight: '320px',
+          borderRadius: '14px',
+          background: 'var(--rad-ui-color-gray-200)',
+          border: '1px solid var(--rad-ui-color-gray-400)',
+          display: 'grid',
+          placeItems: 'center',
+          padding: '40px 28px'
+        }}
+      >
+        <Toolbar.Root aria-label="Formatting options">
+          <Toolbar.ToggleGroup type="multiple" value={textStyles} onValueChange={setTextStyles}>
+            <Toolbar.ToggleItem value="bold" aria-label="Bold">
+              <Bold size={16} />
+            </Toolbar.ToggleItem>
+            <Toolbar.ToggleItem value="italic" aria-label="Italic">
+              <Italic size={16} />
+            </Toolbar.ToggleItem>
+            <Toolbar.ToggleItem value="underline" aria-label="Underline">
+              <Underline size={16} />
+            </Toolbar.ToggleItem>
+          </Toolbar.ToggleGroup>
+          <Toolbar.Separator />
+          <Toolbar.ToggleGroup type="single" value={alignment} onValueChange={setAlignment}>
+            <Toolbar.ToggleItem value="left" aria-label="Align left">
+              <AlignLeft size={16} />
+            </Toolbar.ToggleItem>
+            <Toolbar.ToggleItem value="center" aria-label="Align center">
+              <AlignCenter size={16} />
+            </Toolbar.ToggleItem>
+            <Toolbar.ToggleItem value="right" aria-label="Align right">
+              <AlignRight size={16} />
+            </Toolbar.ToggleItem>
+          </Toolbar.ToggleGroup>
+          <Toolbar.Separator />
+          <Toolbar.Link href="#" aria-label="Insert link">
+            <Link2 size={16} />
+          </Toolbar.Link>
+          <Toolbar.Separator />
+          <span className="rad-ui-toolbar-meta">Edited 2 hours ago</span>
+          <Toolbar.Button aria-label="Share">
+            Share
+          </Toolbar.Button>
+        </Toolbar.Root>
       </div>
     </SandboxEditor>
   );
