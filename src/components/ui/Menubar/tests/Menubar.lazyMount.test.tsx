d7e97f6d8e140185e48cc2e08bdd23c1b7815058
Pranay Kothapalli <pranaykothapalli@gmail.com>
2026-06-24 16:18:13 +0530

    test(Menubar): wrap lazy mount tests with Theme
    Mock matchMedia and render under Theme for portal consistency.

--- src/components/ui/Menubar/tests/Menubar.lazyMount.test.tsx
+++ src/components/ui/Menubar/tests/Menubar.lazyMount.test.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import Theme from '~/components/ui/Theme/Theme';
 import Menubar from '../Menubar';
 
+const mockMatchMedia = () => {
+    if ('matchMedia' in window && typeof window.matchMedia === 'function') return;
+    Object.defineProperty(window, 'matchMedia', {
+        writable: true,
+        value: jest.fn().mockImplementation(() => ({
+            matches: false,
+            addEventListener: jest.fn(),
+            removeEventListener: jest.fn()
+        }))
+    });
+};
+
 describe('Menubar lazy mount', () => {
+    beforeEach(() => mockMatchMedia());
+
     test('does not mount menu content until opened', () => {
         render(
-            <Menubar.Root>
-                <Menubar.Menu>
-                    <Menubar.Trigger>File</Menubar.Trigger>
-                    <Menubar.Portal>
-                        <Menubar.Content data-testid="menubar-content">
-                            <Menubar.Item>New</Menubar.Item>
-                        </Menubar.Content>
-                    </Menubar.Portal>
-                </Menubar.Menu>
-            </Menubar.Root>
+            <Theme>
+                <Menubar.Root>
+                    <Menubar.Menu>
+                        <Menubar.Trigger>File</Menubar.Trigger>
+                        <Menubar.Portal>
+                            <Menubar.Content data-testid="menubar-content">
+                                <Menubar.Item>New</Menubar.Item>
+                            </Menubar.Content>
+                        </Menubar.Portal>
+                    </Menubar.Menu>
+                </Menubar.Root>
+            </Theme>
         );
 
         expect(screen.queryByTestId('menubar-content')).not.toBeInTheDocument();
@@ -26,16 +43,18 @@ describe('Menubar lazy mount', () => {
         const user = userEvent.setup();
 
         render(
-            <Menubar.Root>
-                <Menubar.Menu>
-                    <Menubar.Trigger>File</Menubar.Trigger>
-                    <Menubar.Portal>
-                        <Menubar.Content>
-                            <Menubar.Item>New</Menubar.Item>
-                        </Menubar.Content>
-                    </Menubar.Portal>
-                </Menubar.Menu>
-            </Menubar.Root>
+            <Theme>
+                <Menubar.Root>
+                    <Menubar.Menu>
+                        <Menubar.Trigger>File</Menubar.Trigger>
+                        <Menubar.Portal>
+                            <Menubar.Content>
+                                <Menubar.Item>New</Menubar.Item>
+                            </Menubar.Content>
+                        </Menubar.Portal>
+                    </Menubar.Menu>
+                </Menubar.Root>
+            </Theme>
         );
 
         await user.click(screen.getByText('File'));
